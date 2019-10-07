6d8bd972c5dd451f8d654ba8d5847ef7c150496d
Kristina <kristina.tanya@gmail.com>
2019-10-07 16:56:29 -0700

    In progress, researching how to link foreign key to 2 models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_225840) do
+ActiveRecord::Schema.define(version: 2019_10_07_234302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
