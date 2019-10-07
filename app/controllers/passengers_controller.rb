211930997f0bb0d5a5b7bba7d159989275f58a5c
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-10-07 16:57:30 -0700

    Added passenger.create method

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,18 @@
 class PassengersController < ApplicationController
+  
+  def index
+    @passengers = Passenger.all
+  end
+  
+  def create
+    @passenger = Passenger.new( strongs_params )
+    @passenger.save
+  end
+  
+  
+  private
+  
+  def strongs_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
 end
