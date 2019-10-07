72961c327d927adc469a20330af450d959f31eff
Alice <alicesunhi@gmail.com>
2019-10-07 16:37:17 -0700

    added passenger index + tests

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,99 @@
 class PassengersController < ApplicationController
+  def index
+    @passengers = Passenger.all
+  end
+  
+  def show
+    @passenger = Passenger.find_by(id: params[:id])
+    
+    if @passenger.nil?
+      redirect_to root_path
+      return
+    end
+  end
+  
+  def new
+    @passenger = Passenger.new
+  end
+  
+  def create
+    @passenger = Passenger.new(passenger_params) 
+    if @passenger.save
+      redirect_to passenger_path(@passenger.id)
+      return
+    else 
+      render :new 
+      return
+    end
+  end
+  
+  def edit
+    @passenger = Passenger.find_by(id: params[:id])
+    
+    if @passenger.nil?
+      redirect_to root_path
+      return
+    end
+  end
+  
+  def update
+    @passenger = Passenger.find_by(id: params[:id])
+    
+    if @passenger.nil?
+      redirect_to root_path
+      return
+    end
+    
+    if @passenger.update(passenger_params)
+      redirect_to passenger_path (@passenger.id)
+      return
+    else
+      render :edit
+      return
+    end
+  end
+  
+  def destroy
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
+    
+    if @passenger.nil?
+      redirect_to passengers_path
+      return
+    end
+    
+    @passenger.destroy
+    
+    redirect_to passengers_path
+    return 
+  end
+  
+  def complete
+    @passenger = Passenger.find_by(id: params[:id])
+    
+    if @passenger.nil?
+      redirect_to passengers_path
+      return
+    end
+    
+    if @passenger.completion_date == nil
+      @passenger.completion_date = Time.now
+    else
+      @passenger.completion_date = nil
+    end
+    
+    unless @passenger.save 
+      redirect_to passengers_path
+    end
+    
+    redirect_to passengers_path
+    return 
+  end
+  
+  private
+  
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
+  
 end
--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -1,30 +1,40 @@
 require "test_helper"
 
 describe PassengersController do
-  describe "index" do
-    # Your tests go here
+  describe "index" do  
+    it "responds with success when there are many passengers saved" do
+      test_passenger = Passenger.create(name: "test person", phone_num: "1234567")
+      get passengers_path
+      must_respond_with :success
+    end
+    
+    it "responds with success when there are no passengers saved" do
+      expect(Passenger.count).must_equal 0
+      get passengers_path
+      must_respond_with :success
+    end
   end
-
+  
   describe "show" do
-    # Your tests go here
+    
   end
-
+  
   describe "new" do
     # Your tests go here
   end
-
+  
   describe "create" do
     # Your tests go here
   end
-
+  
   describe "edit" do
     # Your tests go here
   end
-
+  
   describe "update" do
     # Your tests go here
   end
-
+  
   describe "destroy" do
     # Your tests go here
   end
