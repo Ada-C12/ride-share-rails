da1f03cb620d9ce0d8ebe4f8a1d59e0667a904eb
Diana Han <dianah46@gmail.com>
2019-10-07 16:46:43 -0700

    updated trip price column name to cost and ran db:seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_233559) do
+ActiveRecord::Schema.define(version: 2019_10_07_234218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2019_10_07_233559) do
 
   create_table "trips", force: :cascade do |t|
     t.datetime "date"
-    t.float "price"
+    t.float "cost"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
