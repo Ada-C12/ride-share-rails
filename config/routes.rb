e4051080f6245f832e4d4f036003b7076d639496
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-10-07 16:27:00 -0700

    Added index, show, new, create methods to Driver Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    "/drivers"          , to: "drivers#index",   as: :drivers
+  post   "/drivers"          , to: "drivers#create"
+  get    "/drivers/new"      , to: "drivers#new",     as: :new_driver
+  get    "/drivers/:id"      , to: "drivers#show",    as: :driver
+  patch  "/drivers/:id"      , to: "drivers#update"
+  delete "/drivers/:id"      , to: "drivers#destroy"
+  get    "/drivers/:id/edit" , to: "drivers#edit",    as: :edit_driver
+  
 end
