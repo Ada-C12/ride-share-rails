2f412652f5ce708fffd005998930da2dfc43d25d
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-07 16:47:58 -0700

    Drivers#index shows all drivers in database. Visually checked

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,14 @@
 class DriversController < ApplicationController
+  
+  def index
+    @drivers = Driver.all
+  end
+  
+  def new
+    @driver = Driver.new()
+  end
+  
+  def create
+  end
+  
 end
