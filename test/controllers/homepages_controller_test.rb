ffafa5b04bd8584f64b1d72ef471afa6634a5cf9
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-07 16:21:38 -0700

    homepage links work to drivers index, to passengers index, and to trips index.  All tested

--- test/controllers/homepages_controller_test.rb
+++ test/controllers/homepages_controller_test.rb
@@ -3,7 +3,22 @@ require "test_helper"
 describe HomepagesController do
   it "can get the homepage" do
     get root_path
-
+    
+    must_respond_with :success
+  end
+  
+  it "can get to drivers/index" do
+    get drivers_path
+    must_respond_with :success
+  end
+  
+  it "can get to passengers/index" do
+    get passengers_path
+    must_respond_with :success
+  end
+  
+  it "can get to trips/index" do
+    get trips_path
     must_respond_with :success
   end
 end
