3098cccb52bcf30359297328bd93c8a9ce3633b4
daniela sanchez <dnsanche@gmail.com>
2019-10-07 16:11:08 -0700

    Created model for passenger, created migration and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_230043) do
+ActiveRecord::Schema.define(version: 2019_10_07_230900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2019_10_07_230043) do
     t.string "car_make"
     t.string "car_model"
     t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
