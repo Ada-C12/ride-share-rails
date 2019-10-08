233504f4b892e78361c8efe9d0727775f64ca96a
Emily Vomacka <emilyvomacka@gmail.com>
2019-10-08 16:01:56 -0700

    passenger crud ACTUALLY added

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,73 @@
 class PassengersController < ApplicationController
+  def index
+    @passengers = Passenger.all
+  end 
+  
+  def show
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
+    
+    if @passenger.nil? 
+      head :not_found
+      return
+    end 
+  end 
+  
+  def new 
+    @passenger = Passenger.new
+  end 
+  
+  def create 
+    @passenger = Passenger.new(passenger_params)
+    if @passenger.save 
+      redirect_to root_path
+      return 
+    else 
+      render :new
+      return
+    end 
+  end 
+  
+  def edit
+    @passenger = Passenger.find_by(id: params[:id])
+    
+    if @passenger.nil? 
+      head :not_found
+      return
+    end 
+  end 
+  
+  def update 
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.update(passenger_params)
+      #experiment
+      redirect_to passengers_path
+      return
+    else 
+      render :edit
+      return
+    end 
+  end 
+  
+  def destroy 
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
+    
+    if passenger.nil? 
+      head :not_found
+      return
+    end 
+    
+    @passenger.destroy 
+    
+    redirect_to passengers_path
+    return 
+  end 
+  
+  private 
+  
+  def passenger_params 
+    return params.require(:passenger).permit(:name, :phone_num)
+  end 
+  
 end
--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,7 +1,6 @@
 class Driver < ApplicationRecord
   has_many :trips
 
-
   def self.alpha_drivers
     return Driver.order(name: :asc)
   end 
