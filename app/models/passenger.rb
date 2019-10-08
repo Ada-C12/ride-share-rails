031982aeb02531568f40147257a88ac1c8e7a8f1
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-08 15:23:48 -0700

    find_passenger_trips

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,9 @@ class Passenger < ApplicationRecord
   has_many :trips
   # add validations here
   validates :name, :phone, presence: true
+
+  def find_passenger_trips
+    return self.trips.all
+    
+  end
 end
