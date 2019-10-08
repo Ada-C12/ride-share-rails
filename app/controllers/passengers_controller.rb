fe429ee34f748f14103ce52a02913162da14c595
Linnea Johnson <linnea.johnson1@gmail.com>
2019-10-08 16:37:16 -0700

    added actions to passengers_controller, created form partial and new and edit forms

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -13,8 +13,58 @@ class PassengersController < ApplicationController
   end
 
   def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    @passenger = Passenger.new(passenger_params)
+
+    if @passenger.save
+      redirect_to passenger_path(@passenger.id)
+    else
+      render new_passenger_path
+    end
   end
 
   def edit
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger == nil
+      redirect_to passenger_path
+    end
+  end
+
+  def update
+    @passenger = Passenger.find_by(id: params[:id])
+
+    if @passenger == nil
+      redirect_to passengers_path
+      return
+    end
+
+    @passenger.name = params[:passenger][:name]
+    @passenger.phone_num = params[:passenger][:phone_num]
+
+    if @passenger.save
+      redirect_to passenger_path(@passenger.id)
+    end
+  end
+
+  def destroy
+    the_correct_passenger = Passenger.find_by( id: params[:id] )
+
+    if the_correct_passenger.nil?
+      redirect_to passengers_path
+      return
+    else
+      the_correct_passenger.destroy
+      redirect_to root_path
+      return
+    end
+  end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
   end
 end
