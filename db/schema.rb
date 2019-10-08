e473e26b5d335658604c0be654f28890ae83378e
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-08 11:07:05 -0700

    added tests for trip_controller#show

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_10_08_172631) do
-
+  
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
+  
   create_table "drivers", force: :cascade do |t|
     t.string "name"
     t.string "vin"
@@ -24,14 +24,14 @@ ActiveRecord::Schema.define(version: 2019_10_08_172631) do
     t.string "car_make"
     t.string "car_model"
   end
-
+  
   create_table "passengers", force: :cascade do |t|
     t.string "name"
     t.string "phone_num"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
+  
   create_table "trips", force: :cascade do |t|
     t.datetime "date"
     t.integer "rating"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2019_10_08_172631) do
     t.index ["driver_id"], name: "index_trips_on_driver_id"
     t.index ["passenger_id"], name: "index_trips_on_passenger_id"
   end
-
+  
   add_foreign_key "trips", "drivers"
   add_foreign_key "trips", "passengers"
 end
--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -3,20 +3,38 @@ require "test_helper"
 describe TripsController do
   describe "show" do
     # Your tests go here
+    it "responds with success when showing an existing valid trip" do
+      # Ensure that there is a trip saved
+      driver = Driver.create(name: "Bernardo Prosacco", vin: "WBWSS52P9NEYLVDE9")
+      passenger = Passenger.create(name: "test person", phone_num: "1234567")
+      trip = Trip.create(date: DateTime.now, rating: 2, cost: 1000, driver_id: driver.id, passenger_id: passenger.id)
+      
+      # Act
+      get trip_path(trip.id)
+      # Assert
+      must_respond_with :success
+    end
+    
+    it "responds with 404 with an invalid trip id" do
+      # Act
+      get trip_path(-1)
+      # Assert
+      must_respond_with :not_found
+    end
   end
-
+  
   describe "create" do
     # Your tests go here
   end
-
+  
   describe "edit" do
     # Your tests go here
   end
-
+  
   describe "update" do
     # Your tests go here
   end
-
+  
   describe "destroy" do
     # Your tests go here
   end
