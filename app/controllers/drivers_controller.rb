54ab413b6170b61f62cc0408a7e9b2f7ecf52c49
Yasmin <yasmin312002@hotmail.com>
2019-10-08 11:36:56 -0700

    created methods for drivers_controller

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -13,4 +13,60 @@ class DriversController < ApplicationController
       return
     end   
   end
+
+  def new
+    @driver = Driver.new
+  end
+  
+  def create
+    @driver = Driver.new(driver_params) 
+    if @driver.save
+      redirect_to root_path 
+      return
+    else
+      render :new 
+      return
+    end
+  end
+
+  def edit
+    @driver = Driver.find_by(id: params[:id])
+    
+    if @driver.nil?
+      head :not_found
+      return
+    end
+  end
+  
+  def update
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.update(book_params)
+      redirect_to root_path 
+      return
+    else 
+      render :edit 
+      return
+    end
+  end
+  
+  def destroy
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id)
+    
+    if @driver.nil?
+      head :not_found
+      return
+    end
+    
+    @driver.destroy
+    redirect_to drivers_path
+    return
+  end
+  
+  private
+  
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
+
 end 
