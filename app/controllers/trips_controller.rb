3a49275c742b0228d4d182e05b877e3100611433
Alice <alicesunhi@gmail.com>
2019-10-08 11:08:11 -0700

    added trip index and show action/view

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,46 @@
 class TripsController < ApplicationController
+  def index
+    @trips = Trip.all
+  end
+  
+  def show
+    @trip = Trip.find_by(id: params[:id])
+    
+    if @trip.nil?
+      head :not_found
+      return
+    end
+  end
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  def complete
+    
+    
+  end
+  
+  private
+  
+  def trip_params
+    
+  end
+  
 end
