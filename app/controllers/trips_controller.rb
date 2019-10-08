58dfc3dce6f25aebae229829c9cdf97837415ead
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-10-08 14:38:11 -0700

    Added more views for Trips and Passengers

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,16 @@
 class TripsController < ApplicationController
+  
+  def index
+    @trips = Trip.all
+  end
+  
+  def show
+    trip_id = params[:id].to_i
+    @trip = Trip.find_by(id: trip_id)
+    if @trip.nil?
+      redirect_to root_path
+      return
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :homepages
   resources :drivers
   resources :passengers
+  resources :trips
 end
