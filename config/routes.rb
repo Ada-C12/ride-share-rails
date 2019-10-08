5cf435fb23b450dd6d900742eb6643486a1c68a8
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-10-08 11:01:21 -0700

    Added make and model columns to driver model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get    "/drivers"          , to: "drivers#index",   as: :drivers
-  post   "/drivers"          , to: "drivers#create"
-  get    "/drivers/new"      , to: "drivers#new",     as: :new_driver
-  get    "/drivers/:id"      , to: "drivers#show",    as: :driver
-  patch  "/drivers/:id"      , to: "drivers#update"
-  delete "/drivers/:id"      , to: "drivers#destroy"
-  get    "/drivers/:id/edit" , to: "drivers#edit",    as: :edit_driver
+  get    "/drivers", to: "drivers#index", as: :drivers
+  post   "/drivers", to: "drivers#create"
+  get    "/drivers/new", to: "drivers#new", as: :new_driver
+  get    "/drivers/:id", to: "drivers#show", as: :driver
+  patch  "/drivers/:id", to: "drivers#update"
+  delete "/drivers/:id", to: "drivers#destroy"
+  get    "/drivers/:id/edit", to: "drivers#edit", as: :edit_driver
   
 end
