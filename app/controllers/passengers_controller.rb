ab0f8d85d625f64967d5cdc51debeddf7b4f2052
Kristy Hisaw <kristy.hisaw@gmail.com>
2019-10-08 12:23:54 -0700

    added Passengers controller actions show, new, edit, update, create, and delete

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -4,4 +4,78 @@ class PassengersController < ApplicationController
     @passengers = Passenger.all
   end
   
-end
+  def show
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.nil?
+      flash[:error] = "Could not find a passenger with id of #{params[:id]}"
+      redirect_to passengers_path
+      return
+    end
+  end
+  
+  def new
+    @passenger = Passenger.new
+  end
+  
+  def create
+    @passenger = Passenger.new(passenger_params)
+    if @passenger.save
+      redirect_to passengers_path
+      return
+    else
+      render :new
+      return
+    end
+  end
+  
+  def edit
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.nil?
+      flash[:error] = "Could not find passenger"
+      redirect_to passengers_path
+      return
+    end
+  end
+  
+  def update
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.nil?
+      flash[:error] = "Could not find passenger"
+      redirect_to passengers_path
+      return
+    end
+    
+    if @passenger.update(passenger_params)
+      redirect_to passenger_path
+      return
+    else
+      render :new
+      return
+    end
+    
+  end
+  
+  def destroy
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.nil?
+      flash[:error] = "Could not find passenger"
+      redirect_to passengers_path
+      return
+    end
+    
+    if @passenger.destroy
+      redirect_to passengers_path
+      return
+    else
+      render :new
+      return
+    end
+  end
+  
+  private
+  
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+    
+  end
+  
--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,4 +1,4 @@
 class Driver < ApplicationRecord
-  validate :vin, presence: true
-  validate :name, presence: true
+  validates :vin, presence: true
+  validates :name, presence: true
 end
