cad9d53d996d50eba0058ee66b606edc035c2dff
Morgan Schuler <morganlschuler@gmail.com>
2019-10-08 14:33:50 -0700

    Added show page for passenger and show method in controller

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -4,12 +4,19 @@ class PassengersController < ApplicationController
     @passengers = Passenger.all
   end
   
+  def show
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
+  end
   
   
   
   
+  private
   
-  
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
   
   
 end
