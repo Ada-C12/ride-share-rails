68a04fefc1c20be2a4e3b553a5acdb6969599dfe
Janice Huang <janicehuang@gmail.com>
2019-10-07 21:25:50 -0700

    get forms working properly

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -25,41 +25,42 @@ class DriversController < ApplicationController
       render :new
       return
     end
+  end
 
-    def edit
-      @driver = Driver.find_by(id: params[:id])
-      if @driver.nil?
-        redirect_to edit_driver_path
-        return
-      end
-    end
-
-    def update
-      @driver = Driver.find_by(id: params[:id])
-      if @driver.nil?
-        redirect_to drivers_path
-        return
-      end
-      result = @driver.update(task_params)
-      if result
-        redirect_to driver_path(@driver.id)
-      else
-        render :edit
-        return
-      end
-    end
-
-    def destroy
-      driver_id = params[:id]
-      driver = Driver.find_by(id: driver_id)
-      if driver.nil?
-        head :not_found
-        return
-      end
-    end
-
-    private
-    def driver_params
-      return params.require(:driver).permit(:name, :vin, :active, :car_make, :car_model)
+  def edit
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      redirect_to edit_driver_path
+      return
     end
   end
+
+  def update
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      redirect_to drivers_path
+      return
+    end
+    result = @driver.update(driver_params)
+    if result
+      redirect_to driver_path(@driver.id)
+    else
+      render :edit
+      return
+    end
+  end
+
+  def destroy
+    driver_id = params[:id]
+    driver = Driver.find_by(id: driver_id)
+    if driver.nil?
+      head :not_found
+      return
+    end
+  end
+
+  private
+  def driver_params
+    return params.require(:driver).permit(:name, :vin, :active, :car_make, :car_model)
+  end
+end
