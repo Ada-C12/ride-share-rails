e1fd2797e8c26f5c3fb8fc446143824854455475
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-10-08 14:06:18 -0700

    Added passengers views

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -3,6 +3,17 @@ class PassengersController < ApplicationController
   def index
     @passengers = Passenger.all
   end
+    
+  def show
+    passenger_id = params[:id].to_i
+    @passenger = Passenger.find_by(id: passenger_id)
+    if @passenger.nil?
+      redirect_to root_path
+      return
+    end
+    total_charges = @passenger.trips.map {|t| t.cost }
+    @total_charges = total_charges.sum
+  end
   
   def create
     @passenger = Passenger.new( strongs_params )
