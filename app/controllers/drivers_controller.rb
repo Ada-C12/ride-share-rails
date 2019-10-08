db892f878fe1f116d484eacc75f508b035368a7a
Yasmin <yasmin312002@hotmail.com>
2019-10-07 18:43:33 -0700

    created homepage and add routes for passengers and homepage

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,7 +1,7 @@
 class DriversController < ApplicationController
   
   def index
-    @drivers = Driver.all  
+    @drivers = Driver.all 
   end
   
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   # RESTful routes:
-  root 'drivers#index'
+  root 'homepage#index'
+  #root 'passengers#index'
   resources :drivers
+  resources :passengers
   
   # 'author/:id', to: 'author#show', as: 'author'
 end
