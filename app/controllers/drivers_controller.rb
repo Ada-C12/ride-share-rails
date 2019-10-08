3b325ad4dcf8f8a170c784bd86ccffbdc5a69c41
gracexinran <gracexinran@gmail.com>
2019-10-07 17:05:06 -0700

    created routes, view files, added nav bar

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,21 @@
 class DriversController < ApplicationController
+  def index
+    
+  end 
+  
+  def show 
+  end 
+  
+  def new 
+  end 
+  def create 
+  end 
+  def edit 
+  end 
+  def update 
+  end 
+  def destroy 
+  end 
+  
+  
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,18 @@
 class TripsController < ApplicationController
+    def index 
+    end 
+
+    def show 
+    end 
+
+    def new 
+    end 
+    def create 
+    end 
+    def edit 
+    end 
+    def update 
+    end 
+    def destroy 
+    end 
 end
