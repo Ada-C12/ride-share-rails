6579471a668f89f05e5693928499d8fe7581ed12
KKennedyCodes <holmstedtk@gmail.com>
2019-10-08 11:18:07 -0700

    Created Models

---
+++ db/migrate/20191008181751_create_trips.rb
@@ -0,0 +1,13 @@
+class CreateTrips < ActiveRecord::Migration[5.2]
+  def change
+    create_table :trips do |t|
+      t.date, :date
+      t.float, :cost
+      t.integer, :rating
+      t.integer, :passenger_id
+      t.integer :driver_id
+
+      t.timestamps
+    end
+  end
+end
