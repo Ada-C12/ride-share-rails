2c250d9f02145063577a8ba52c4e7b69b78cfa3a
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-10-08 15:34:24 -0700

    Refactored link list of trips for driver and passenger to now link to trip details.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,5 +3,17 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
   end
+
+  def show
+    trip_id = params[:id]
+    @trip = Trip.find_by(id: trip_id)
+  end
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:driver_id, :passenger_id, :date, :rating, :cost)
+  end
+
   
 end
