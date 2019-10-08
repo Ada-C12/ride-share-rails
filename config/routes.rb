65428c57d22e96a43af063107e0d35fd64ee493b
Morgan Schuler <morganlschuler@gmail.com>
2019-10-08 14:23:01 -0700

    Added index and view page for drivers

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :passengers
   resources :drivers
-  resources :trips
+  resources :trips, except: [:index]
 end
