1179b3185e348b854f3cfa485662a52d10f17bd8
Linnea Johnson <linnea.johnson1@gmail.com>
2019-10-08 16:07:14 -0700

    Seeded database, added passengers show page

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -4,7 +4,12 @@ class PassengersController < ApplicationController
   end
 
   def show
-    
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
+    if @passenger.nil?
+      redirect_to passengers_path
+      return
+    end
   end
 
   def new
