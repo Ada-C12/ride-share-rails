175b58d2e89bd7c44abe9cb0d5c4d6ddf1e9ecff
m.dove@live.com <m.dove@live.com>
2019-10-07 22:34:59 -0700

    added new passenger and new driver methods with matching views

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -5,11 +5,29 @@ class DriversController < ApplicationController
 
   def show
     driver_id = params[:id].to_i
-    @driver = Driver.find_by(id:driver_id)
+    @driver = Driver.find_by(id: driver_id)
     if @driver.nil?
       redirect_to drivers_path
       return
     end
   end
-  
+
+  def create
+    @driver = Driver.new(driver_params)
+    if @driver.save
+      redirect_to driver_path(@driver.id)
+    else
+      render new_driver_path
+    end
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
 end
