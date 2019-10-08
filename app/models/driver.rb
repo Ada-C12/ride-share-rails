6bf5e437540951412c5281df39b03b71f8e1229e
KKennedyCodes <holmstedtk@gmail.com>
2019-10-08 12:23:07 -0700

    Added Models, Seeded Database, and Verified Seeding

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,7 @@
 class Driver < ApplicationRecord
   has_many :trips
+  
+  ##Validation Code
+  #validates :name, presence: true;
+  #validates :vin presence: true, uniqueness: true
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,7 @@
 class Passenger < ApplicationRecord
   has_many :trips
+  
+  # #Validation Code
+  # validates :name, presence: true;
+  # validates :phone_num, presence: true, uniqueness: true
 end
