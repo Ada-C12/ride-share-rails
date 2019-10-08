3ba23e8f2f82c1e1f875fd5758998b9115af54b4
Alice <alicesunhi@gmail.com>
2019-10-08 16:15:47 -0700

    typo

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -27,59 +27,58 @@ class DriversController < ApplicationController
     else 
       render :new
       return 
-      endt
     end
-    
-    
-    def edit
-      driver_id = params[:id].to_i
-      @driver = Driver.find_by(id: driver_id)
-      
-      if @driver.nil?
-        redirect_to drivers_path
-        return
-      end
-    end
-    
-    def update
-      driver_id = params[:id].to_i
-      @driver = Driver.find_by(id: driver_id)
-      
-      if @driver.nil?
-        head :not_found
-        return
-      elsif @driver.update(driver_params)
-        redirect_to driver_path(@driver.id)
-        return
-      else
-        render :edit
-        return
-      end
-    end
-    
-    def destroy
-      driver_id = params[:id].to_i
-      @driver = Driver.find_by(id: driver_id)
-      
-      if @driver.nil?
-        head :not_found
-        return
-      else
-        @driver.destroy
-        redirect_to drivers_path
-        return
-      end
-    end
-    
-    private
-    
-    def driver_params
-      if !params[:driver].nil?
-        return params.require(:driver).permit(:name, :vin, :active, :car_make, :car_model)
-      else
-        return nil
-      end
-    end
-    
   end
   
+  
+  def edit
+    driver_id = params[:id].to_i
+    @driver = Driver.find_by(id: driver_id)
+    
+    if @driver.nil?
+      redirect_to drivers_path
+      return
+    end
+  end
+  
+  def update
+    driver_id = params[:id].to_i
+    @driver = Driver.find_by(id: driver_id)
+    
+    if @driver.nil?
+      head :not_found
+      return
+    elsif @driver.update(driver_params)
+      redirect_to driver_path(@driver.id)
+      return
+    else
+      render :edit
+      return
+    end
+  end
+  
+  def destroy
+    driver_id = params[:id].to_i
+    @driver = Driver.find_by(id: driver_id)
+    
+    if @driver.nil?
+      head :not_found
+      return
+    else
+      @driver.destroy
+      redirect_to drivers_path
+      return
+    end
+  end
+  
+  private
+  
+  def driver_params
+    if !params[:driver].nil?
+      return params.require(:driver).permit(:name, :vin, :active, :car_make, :car_model)
+    else
+      return nil
+    end
+  end
+  
+end
