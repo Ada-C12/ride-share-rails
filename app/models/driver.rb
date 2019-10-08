0d51dea8df0003fc91ce5eea5a7a8b8f4c8a4092
Kristina <kristina.tanya@gmail.com>
2019-10-08 15:01:18 -0700

    Listed trips for an individual driver show page.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,4 +1,13 @@
 class Driver < ApplicationRecord
   has_many :trips
   validates :name, :vin, presence: true
+  
+  def find_driver_trips
+    return self.trips.all
+    # driver_trips = self.trips.where({id: driver_id})
+    # return driver_trips
+    
+  end
+  
+  
 end
