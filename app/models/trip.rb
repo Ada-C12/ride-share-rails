5b647cb88bf12283963c3d4b7978c83c3afd5fa0
Kristina <kristina.tanya@gmail.com>
2019-10-08 09:57:35 -0700

    Added foreign key validation for Driver and Passenger

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,6 @@
 class Trip < ApplicationRecord
-  belongs_to :driver,
-  belongs_to :passenger,
+  belongs_to :driver
+  belongs_to :passenger
+  validates :driver, :presence => true
+  validates :passenger, :presence => true
 end
