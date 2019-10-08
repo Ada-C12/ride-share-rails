792ee407e35632d9dc63796b54b190502197d297
Brianna Kemp <kemp.bri@gmail.com>
2019-10-08 12:25:52 -0700

    Removed incorrect column and updated drivers controller

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,18 @@
 class DriversController < ApplicationController
+  
+  def index
+    @drivers = Driver.all
+  end
+  
+  def create
+    @driver = Driver.new( strongs_params )
+    @driver.save
+  end
+  
+  
+  private
+  
+  def strongs_params
+    return params.require(:driver).permit(:name, :vin)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_231618) do
+ActiveRecord::Schema.define(version: 2019_10_08_191844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2019_10_07_231618) do
   create_table "drivers", force: :cascade do |t|
     t.string "name"
     t.string "vin"
-    t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
