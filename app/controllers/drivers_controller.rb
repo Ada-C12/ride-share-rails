287c2e9f027d6a36db940c10f2f75598213948d0
Sara Shah Baig <sarashahbaig@gmail.com>
2019-10-08 16:04:18 -0700

    data loaded by seed

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,16 +1,17 @@
 class DriversController < ApplicationController
 
     def index
-        @drivers = Driver.all.order(:id)
-        end
+        @drivers = Driver.all
+    end
 
     def show
         driver_id = params[:id].to_i
-        @driver = Driver.find_by(id:driver)
+        @driver = Driver.find_by(id:driver_id)
     
     end
 
     def new
+        @driver = Driver.new
     end
 
     def edit
