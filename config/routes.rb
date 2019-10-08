0d93dec4729c91efd37b9417d54e19ae722c917e
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-10-08 14:35:09 -0700

    Added methods index, show, new, create to Passenger

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   patch  "/drivers/:id", to: "drivers#update"
   delete "/drivers/:id", to: "drivers#destroy"
   get    "/drivers/:id/edit", to: "drivers#edit", as: :edit_driver
-  
+
+  get    "/passengers", to: "passengers#index", as: :passengers
+  post   "/passengers", to: "passengers#create"
+  get    "/passengers/new", to: "passengers#new", as: :new_passenger
+  get    "/passengers/:id", to: "passengers#show", as: :passenger
+  patch  "/passengers/:id", to: "passengers#update"
+  delete "/passengers/:id", to: "passengers#destroy"
+  get    "/passengers/:id/edit", to: "passengers#edit", as: :edit_passenger
 end
