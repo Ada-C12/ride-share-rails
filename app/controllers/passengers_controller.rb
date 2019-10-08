2875c0a5af1b7aec963293aa25f2653762a3a2ab
KKennedyCodes <holmstedtk@gmail.com>
2019-10-08 12:33:11 -0700

    Added Content to Index Method

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,8 +1,17 @@
 class PassengersController < ApplicationController
   def index
+    @passengers = Passenger.all
   end
   
   def show
+    # passenger_id = params[:id]
+    # @passenger = Passenger.find_by(id: passenger_id)
+    # if @passenger.nil?
+    #   head :not_found
+    #   return
+    # end
+    
+    #Should this be @passengers? Link: https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/08-rails/using-active-record-in-code.md
   end
   
   def edit
