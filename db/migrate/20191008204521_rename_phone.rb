a0de5c4315277ec3175f29c4269f89143a3f92ca
Mira <calopter@gmail.com>
2019-10-08 13:48:38 -0700

    renames phone to phone_num

---
+++ db/migrate/20191008204521_rename_phone.rb
@@ -0,0 +1,5 @@
+class RenamePhone < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :passengers, :phone, :phone_num
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_204111) do
+ActiveRecord::Schema.define(version: 2019_10_08_204521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2019_10_08_204111) do
   end
 
   create_table "passengers", force: :cascade do |t|
-    t.string "phone"
+    t.string "phone_num"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
