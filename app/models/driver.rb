42d7f825585fe9e521e38f117b6a2c2478b80e39
Eve Le <nguyenthao.lee@gmail.com>
2019-10-08 15:41:59 -0700

    Add input validation test for drivers#create action

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,6 @@
 class Driver < ApplicationRecord
     has_many :trips
+
+    validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+    validates :vin, presence: true, uniqueness: true
 end
