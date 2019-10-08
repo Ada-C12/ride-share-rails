633fff454dada87f75e3311a5c5ca042e4114d0d
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-10-08 15:56:20 -0700

    Wrote total_amount for the passenger model and total_earned for the driver model. Implemented each in their show page.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,4 +1,14 @@
 class Driver < ApplicationRecord
   
   has_many :trips
+
+  def total_earned
+    total = 0
+
+    self.trips.each do |trip|
+      cost = trip.cost
+      total += cost
+      end
+      return '%.2f' % total
+    end
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,5 +1,15 @@
 class Passenger < ApplicationRecord
   
   has_many :trips
-  
+
+  def total_amount
+    total = 0
+
+    self.trips.each do |trip|
+      cost = trip.cost
+      total += cost
+      end
+      return '%.2f' % total
+    end
+   
 end
