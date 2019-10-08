576c3bace7f1e64dcd193e27d52df4311efdc288
Nicky Choi <nickyjinchoi@gmail.com>
2019-10-07 20:38:49 -0700

    Created index, new, show, and partial view files and generated Driver model

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,37 +1,36 @@
 class DriversController < ApplicationController
-  
   def index
     @drivers = Driver.all
   end
-  
+
   def show
     @driver = Driver.find_by(id: params[:id])
-    
+
     if @driver.nil?
       head :not_found
       return
     end
   end
-  
+
   def new
     @driver = Driver.new
   end
-  
+
   def create
     @driver = Driver.new(driver_params)
-    
+
     if @driver.save
-      redirect_to driver_path
+      redirect_to driver_path(@driver)
+      return
     else
       render "new"
       return
     end
   end
-  
+
   private
-  
+
   def driver_params
     return params.require(:driver).permit(:name, :vin, :available)
   end
-  
 end
