af0596474cd6fc4ebe62e1a3337a12b3b8178c26
Sara Shah Baig <sarashahbaig@gmail.com>
2019-10-08 16:45:32 -0700

    edit,update and create updated got the driver file

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,12 +1,21 @@
 class DriversController < ApplicationController
 
     def index
-        @drivers = Driver.all
+        @drivers = Driver.all.order(:id)
     end
 
     def show
         driver_id = params[:id].to_i
         @driver = Driver.find_by(id:driver_id)
+
+        if driver_id < 0
+            redirect_to root_path
+        end
+
+        if @drive.nil?
+            redirect_to new_driver_path
+            return
+        end
     
     end
 
@@ -14,7 +23,35 @@ class DriversController < ApplicationController
         @driver = Driver.new
     end
 
+    def create
+        @driver = Driver.new(driver_params)
+        if @driver.save
+            redirect_to driver_path(@driver.id)
+        else
+            render new_driver_path
+        end
+    end
+
     def edit
+        @driver = Driver.find_by(id: id)
+        id = params[:id].to_i
+    end
+
+    def update
+        id = params[:id].to_i
+        if id < 0
+            redirect_to root_path
+        end
+
+        @driver = Driver.find_by(id: id)
+        @driver[:name] = params[:driver][:name]
+        @driver[:vin] = params[:driver][:vin]
+
+        if @driver.save
+            redirect_to driver_path(@driver.id)
+        else
+            render new_driver_path
+        end
     end
 
 
