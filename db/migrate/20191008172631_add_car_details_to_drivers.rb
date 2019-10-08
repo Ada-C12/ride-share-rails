9678400b0ea004472e14c61eb82d06f55505eea1
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-08 10:28:44 -0700

    migrated to add active, car make, and car model to drivers table

---
+++ db/migrate/20191008172631_add_car_details_to_drivers.rb
@@ -0,0 +1,8 @@
+class AddCarDetailsToDrivers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :drivers, :active, :boolean
+    
+    add_column :drivers, :car_make, :string
+    add_column :drivers, :car_model, :string
+  end
+end
