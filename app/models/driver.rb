a25f4953c8ea2dff71ba7690976af712f2b5d8da
Emily Vomacka <emilyvomacka@gmail.com>
2019-10-08 11:27:23 -0700

    faker cars in db

---
+++ app/models/driver.rb
@@ -0,0 +1,8 @@
+class Driver < ApplicationRecord
+  has_many :trips
+
+
+  def self.alpha_drivers
+    return Driver.order(name: :asc)
+  end 
+end
---
+++ app/models/passenger.rb
@@ -0,0 +1,7 @@
+class Passenger < ApplicationRecord
+  has_many :trips
+
+  def self.alpha_passengers
+    return Passenger.order(name: :asc)
+  end 
+end
