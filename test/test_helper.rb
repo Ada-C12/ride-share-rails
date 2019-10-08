1ee17f6013edcf469896ce7b6d6ad2d371d31deb
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-10-08 11:09:44 -0700

    Additions to test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,17 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
+ENV['RAILS_ENV'] = 'test'
+require File.expand_path('../config/environments', __FILE__)
 require 'rails/test_help'
+
+require 'minitest/rails'
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+Minitest::Reporters.use!(
+  Minitest::Reporters::SpecReporter.new,
+  ENV,
+  Minitest.backtrace_filter
+)
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
