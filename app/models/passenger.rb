590909792c4af9e4797a4ef698de51be34db30a3
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-08 16:04:50 -0700

    amount_charged method

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -5,6 +5,13 @@ class Passenger < ApplicationRecord
 
   def find_passenger_trips
     return self.trips.all
-    
+  end
+
+  def amount_charged
+    total = 0
+    self.trips.all.each do |trip|
+      total += trip.cost
+    end
+    return total
   end
 end
