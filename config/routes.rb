f896f76b998cce2aab16831ea4865deb9968c5dc
Farah Davoodi <farahdavoodi@gmail.com>
2019-10-08 16:52:26 -0700

    updated driver views and added more tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   root :to => 'homepages#index'
-
+  
+  resources :passengers
+  
   resources :drivers
   
 end
