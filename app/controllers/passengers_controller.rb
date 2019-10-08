db56990ccbadcc6f33e3e75601ec4dad25758ae5
brilatimer <brihiramine@gmail.com>
2019-10-07 22:14:43 -0700

    main list of all passengers, when clicked takes you to details with phone number, edit and delete functions. Return to passengers option

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,4 +2,29 @@ class PassengersController < ApplicationController
   def index
     @passengers = Passenger.all 
   end
+  
+  def destroy
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
+    
+    if @passenger.nil?
+      head :not_found
+      return
+    end
+    
+    @passenger.destroy
+    
+    redirect_to root_path
+    return
+  end
+  
+  def show
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
+    
+    if @passenger.nil?
+      redirect_to passenger_path
+      return
+    end
+  end
 end
