f0b72dd1fe3e3f9abd4f8e38d6f37f9c8490500c
Monick <monick.keo@gmail.com>
2019-10-08 13:52:05 -0700

    Added tests for create and new action for driver_controller and added view

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -10,7 +10,6 @@ class DriversController < ApplicationController
   
   def show
     driver_id = params[:id].to_i
-    # Both find and find_by will work... But Dee has a preference for find_by. Why?
     @driver = Driver.find_by(id: driver_id)
     if @driver.nil?
       head :not_found
@@ -18,7 +17,50 @@ class DriversController < ApplicationController
     end
   end 
 
+  def new
+    @driver = Driver.new
+  end 
+
   def create
+    @driver = Driver.new(driver_params)
+
+    if @driver.save
+      redirect_to driver_path(@driver.id)
+    else
+      render new_driver_path
+    end
   end
-  
+
+  def edit
+    @driver = Driver.find_by(id:params[:id])
+  end 
+
+  def update
+    @driver = Driver.find_by(id:params[:id])
+
+    if @driver.update(driver_params)
+      redirect_to driver_path(@driver.id)
+    else
+      render new_driver_path
+    end
+  end 
+
+  def destroy
+    the_driver_account = Driver.find_by(id: params[:id])
+
+    if the_driver_account.nil?
+      redirect_to drivers_path
+      return
+    else
+      the_driver_account.destroy
+      redirect_to root_path
+      return
+    end
+  end 
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
 end
