9642a6154e04cb4c55edeca58dfc3b3bb82f6ef3
Vi <vanessareigosa@gmail.com>
2019-10-08 13:45:24 -0700

    Setup/baseline index views for our models and controllers

---
+++ app/models/driver.rb
@@ -0,0 +1,5 @@
+class Driver < ApplicationRecord
+  has_many :trips
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  root to: "homepages#index"
+
+  resources :drivers
+  resources :passengers
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
