332d2c96e9d88e0fab8711b40d895307fb8e46d3
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-10-08 14:06:04 -0700

    started routes and nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # root :to "determine a root thing, sometime"
+  
+  resources :trips
+  
+  resources :drivers
+  
+  resources :passengers
+  
 end
