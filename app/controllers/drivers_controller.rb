730fc7864320c8df60fc0246c410cb4b3dca50df
daniela sanchez <dnsanche@gmail.com>
2019-10-08 15:39:10 -0700

    Added drivers show view and connected all trips to driver

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -2,4 +2,12 @@ class DriversController < ApplicationController
   def index
     @drivers = Driver.alpha_drivers
   end
+
+  def show
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      redirect_to root_path
+    return 
+    end
+  end
 end
