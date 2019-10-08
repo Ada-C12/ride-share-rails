da2adab5df50bf4eb6b50b5754153fdc21675ed0
Julia Bouvier <juliabouv@gmail.com>
2019-10-08 12:43:18 -0700

    Created Homepage controller. Implemented index and show actions for Passenger and Driver controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homepages#index'
+
+
+  resources :drivers, only: [:index, :show]
+
+  resources :passengers, only: [:index, :show]
+
 end
