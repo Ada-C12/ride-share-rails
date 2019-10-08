f02f495061417da116800ab1047077bb92fe9274
KKennedyCodes <holmstedtk@gmail.com>
2019-10-08 12:28:17 -0700

    Passengers, Drivers, Trips Controllers Created

---
+++ app/controllers/drivers_controller.rb
@@ -0,0 +1,21 @@
+class DriversController < ApplicationController
+  def index
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    #Handle Validation Errors
+  end
+  
+  def new
+  end
+  
+  def create
+    #Handle Validation Errors
+  end
+end
---
+++ app/controllers/passengers_controller.rb
@@ -0,0 +1,21 @@
+class PassengersController < ApplicationController
+  def index
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    #Handle Validation Errors
+  end
+  
+  def new
+  end
+  
+  def create
+    #Handle Validation Errors
+  end
+end
