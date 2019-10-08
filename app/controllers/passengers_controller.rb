012a87e86bf6424b815fc82c0e87c02b6213fc29
m.dove@live.com <m.dove@live.com>
2019-10-07 22:40:08 -0700

    Oops - added the actions for new passenger this time

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -5,13 +5,29 @@ class PassengersController < ApplicationController
 
   def show
     passenger_id = params[:id].to_i
-    @passenger = Passenger.find_by(id:passenger_id)
+    @passenger = Passenger.find_by(id: passenger_id)
     if @passenger.nil?
       redirect_to passengers_path
       return
     end
-
   end
 
+  def create
+    @passenger = Passenger.new(passenger_params)
+    if @passenger.save
+      redirect_to passenger_path(@passenger.id)
+    else
+      render new_passenger_path
+    end
+  end
 
+  def new
+    @passenger = Passenger.new
+  end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
 end
