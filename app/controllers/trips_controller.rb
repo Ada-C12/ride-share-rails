d044ea38e7827bb43f58a1e9275d473e8022c812
Nicky Choi <nickyjinchoi@gmail.com>
2019-10-08 16:17:59 -0700

    Added create method to Trip class

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,20 @@
 class TripsController < ApplicationController
+  def create
+    if params[:driver_id] && params[:passenger_id]
+      @trip = Trip.new(trip_params)
+      if @trip.save
+        redirect_to passenger_path(params[:passenger_id])
+        return
+      else
+        render "new"
+        return
+      end
+    end
+  end
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:date, :rating, :cost, :driver_id, :passenger_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,26 @@
 Rails.application.routes.draw do
   root "homepages#index"
 
-  get "/drivers", to: "drivers#index", as: :drivers
-  post "/drivers", to: "drivers#create"
-  get "/drivers/new", to: "drivers#new", as: :new_driver
-  get "/drivers/:id", to: "drivers#show", as: :driver
-  patch "/drivers/:id", to: "drivers#update"
-  delete "/drivers/:id", to: "drivers#destroy"
-  get "/drivers/:id/edit", to: "drivers#edit", as: :edit_driver
+  resources :drivers
+  # get "/drivers", to: "drivers#index", as: :drivers
+  # post "/drivers", to: "drivers#create"
+  # get "/drivers/new", to: "drivers#new", as: :new_driver
+  # get "/drivers/:id", to: "drivers#show", as: :driver
+  # patch "/drivers/:id", to: "drivers#update"
+  # delete "/drivers/:id", to: "drivers#destroy"
+  # get "/drivers/:id/edit", to: "drivers#edit", as: :edit_driver
 
-  get "/passengers", to: "passengers#index", as: :passengers
-  post "/passengers", to: "passengers#create"
-  get "/passengers/new", to: "passengers#new", as: :new_passenger
-  get "/passengers/:id", to: "passengers#show", as: :passenger
-  patch "/passengers/:id", to: "passengers#update"
-  delete "/passengers/:id", to: "passengers#destroy"
-  get "/passengers/:id/edit", to: "passengers#edit", as: :edit_passenger
+  resources :passengers do
+    resources :trips, only: [:create]
+  end
+
+  # get "/passengers", to: "passengers#index", as: :passengers
+  # post "/passengers", to: "passengers#create"
+  # get "/passengers/new", to: "passengers#new", as: :new_passenger
+  # get "/passengers/:id", to: "passengers#show", as: :passenger
+  # patch "/passengers/:id", to: "passengers#update"
+  # delete "/passengers/:id", to: "passengers#destroy"
+  # get "/passengers/:id/edit", to: "passengers#edit", as: :edit_passenger
 
   get "/trips", to: "trips#index", as: :trips
 end
