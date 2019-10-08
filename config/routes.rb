90cb5ada2d7b5ee793c4dcea2a46f17cca2f6b34
Kristina <kristina.tanya@gmail.com>
2019-10-08 09:13:40 -0700

    Changed pages route from home to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  root to: 'pages#home'
+  root to: 'pages#index'
   resources :trips
   resources :drivers 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
