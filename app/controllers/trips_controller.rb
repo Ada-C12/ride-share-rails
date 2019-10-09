e4477cbc5bffd07e5cc898782c68a263cfdfff55
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-10-08 20:37:23 -0700

    Added new, create, update, and destroy to Trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,7 +2,6 @@ class TripsController < ApplicationController
   
   def index
     @trips = Trip.all
-    @drivers = Driver.all
   end
   
   def show
@@ -20,22 +19,56 @@ class TripsController < ApplicationController
   
   def create 
     @trip = Trip.new( trip_params )
-    if @trip.date == nil
+    if @trip.date.nil?
       @trip.date = Date.today
     end
-    if @trip.cost == nil
+    if @trip.cost.nil?
       @trip.cost = 0
     end
-    @trip.rating = 0
+    
+    @drivers = Driver.all
     @trip.driver_id = @drivers[rand(0...@drivers.length)].id
-    @trip.save
+    
+    if @trip.save
+      redirect_to trip_path(@trip.id)
+      return
+    else
+      redirect_to root_path
+      return
+    end
+  end
+  
+  def edit
+    @trip = Trip.find_by(id: params[:id] )
+  end
+  
+  def update
+    @trip = Trip.find_by(id: params[:id] )
+    if @trip.update( trip_params )
+      redirect_to trip_path(@trip.id)
+      return
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    trip = Trip.find_by( id: params[:id] )
+    if trip.nil?
+      redirect_to trips_path
+      return
+    else
+      trip.destroy
+      redirect_to trips_path
+      return
+    end
   end
   
   
   private
   
   def trip_params
-    return params.require(:trip).permit(:passenger_id, :driver_id, :date => Date.today, :rating => 0, :cost => 0)
+    return params.require(:trip).permit(:passenger_id, :driver_id, :date, :rating, :cost)
     
   end
   
