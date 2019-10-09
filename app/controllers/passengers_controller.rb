a71ee29899baf3bf41dfacce5b6c37b093818903
Dianna Tingg <dtingg@gmail.com>
2019-10-09 15:28:20 -0700

    added index new and show for passenger

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,5 +1,73 @@
 class PassengersController < ApplicationController
   
   def index
+    @passengers = Passenger.order(:name)
+  end
+  
+  def show
+    @passenger = Passenger.find_by(id: params[:id])
+    
+    if @passenger.nil?
+      head :not_found
+      return
+    end
+  end
+  
+  def new
+    @passenger = Passenger.new
+  end
+  
+  def create
+    if params[:passenger].nil?
+      redirect_to new_passenger_path
+    end
+    
+    @passenger = Passenger.new(passenger_params)
+    
+    if @passenger.save
+      redirect_to passenger_path(@passenger.id)
+      return
+    else
+      redirect_to new_passenger_path
+      return
+    end
+  end
+  
+  def edit
+    @passenger = Passenger.find_by(id: params[:id])
+    
+    if @passenger.nil?
+      redirect_to passengers_path
+      return
+    end 
+  end
+  
+  def update
+    @passenger = Passenger.find_by(id: params[:id])
+    
+    if @passenger.nil?
+      head :not_found
+      return
+    elsif @passenger.update(passenger_params)
+      redirect_to passenger_path
+      return
+    else
+      render :edit
+      return
+    end
+  end
+  
+  def destroy
+    @passenger = Passenger.find_by(id: params[:id])
+    
+    @passenger.destroy if @passenger
+    
+    redirect_to passengers_path
+  end
+  
+  private
+  
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
   end
 end
--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -1,30 +1,76 @@
 require "test_helper"
 
 describe PassengersController do
+  let (:passenger) {
+    Passenger.create(name: "Barney Rubble", phone_num: "123-456-7890") 
+  }
   describe "index" do
-    # Your tests go here
+    it "responds with success when there are many passengers saved" do      
+      test_passenger = passenger
+      
+      # Act
+      get passengers_path
+      
+      # Assert
+      must_respond_with :success   
+    end
+    it "responds with success when there are no passengers saved" do
+      # Arrange
+      # Ensure that there are zero passengers saved
+      
+      # Act
+      get passengers_path
+      
+      # Assert
+      must_respond_with :success
+    end
   end
-
+  
   describe "show" do
-    # Your tests go here
+    it "responds with success when showing an existing valid passenger" do
+      # Arrange
+      # Ensure that there is a driver saved
+      test_passenger = passenger
+      
+      # Act
+      get passenger_path(passenger.id)
+      
+      # Assert
+      must_respond_with :success
+    end
+    
+    it "responds with 404 with an invalid driver id" do
+      # Arrange
+      # Ensure that there is an id that points to no driver
+      
+      # Act
+      get passenger_path(-1)
+      
+      # Assert
+      must_respond_with :not_found
+    end
   end
-
+  
   describe "new" do
-    # Your tests go here
+    it "responds with success" do
+      get new_passenger_path
+      
+      must_respond_with :success
+    end
   end
-
+  
   describe "create" do
     # Your tests go here
   end
-
+  
   describe "edit" do
     # Your tests go here
   end
-
+  
   describe "update" do
     # Your tests go here
   end
-
+  
   describe "destroy" do
     # Your tests go here
   end
