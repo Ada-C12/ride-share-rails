9804d7a8ce0a1ea6c77aa9558aa6a6add2dd405c
Vi <vanessareigosa@gmail.com>
2019-10-08 20:00:20 -0700

    added error handling and nice labels

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,6 +1,9 @@
 class Driver < ApplicationRecord
   has_many :trips
 
+  validates :name, presence: true
+  validates :vin, presence: true
+
   def self.total_earnings
   # The driver gets 80% of the trip cost after a fee of $1.65 is subtracted
     
--- test/models/passenger_test.rb
+++ test/models/passenger_test.rb
@@ -1,5 +1,5 @@
 require "test_helper"
-​
+
 describe Passenger do
   let (:new_passenger) {
     Passenger.new(name: "Kari", phone_num: "111-111-1211")
@@ -9,7 +9,7 @@ describe Passenger do
     # Assert
     expect(new_passenger.valid?).must_equal true
   end
-​
+
   it "will have the required fields" do
     # Arrange
     new_passenger.save
@@ -19,13 +19,12 @@ describe Passenger do
       expect(passenger).must_respond_to field
     end
   end
-​
+
   describe "relationships" do
     it "can have many trips" do
       # Arrange
       new_passenger.save
       passenger = Passenger.first
-​
       # Assert
       expect(passenger.trips.count).must_be :>, 0
       passenger.trips.each do |trip|
@@ -33,35 +32,34 @@ describe Passenger do
       end
     end
   end
-​
+
   describe "validations" do
     it "must have a name" do
       # Arrange
       new_passenger.name = nil
-​
       # Assert
       expect(new_passenger.valid?).must_equal false
       expect(new_passenger.errors.messages).must_include :name
       expect(new_passenger.errors.messages[:name]).must_equal ["can't be blank"]
     end
-​
+
     it "must have a phone number" do
       # Arrange
       new_passenger.phone_num = nil
-​
+
       # Assert
       expect(new_passenger.valid?).must_equal false
       expect(new_passenger.errors.messages).must_include :phone_num
       expect(new_passenger.errors.messages[:phone_num]).must_equal ["can't be blank"]
     end
   end
-​
+
   # Tests for methods you create should go here
   describe "custom methods" do
     describe "request a ride" do
       # Your code here
     end
-​
+
     describe "complete trip" do
       # Your code here
     end
