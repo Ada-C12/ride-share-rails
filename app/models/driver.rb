2d7337807a05edf866f33d7247e7da2abe229e38
Samantha Coll <samcoll@Samanthas-MBP.localdomain>
2019-10-09 15:47:56 -0700

    updating relationships

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,11 +1,11 @@
 class Driver < ApplicationRecord
-<<<<<<< HEAD
-
+  has_many :trips
+  
   def total_earnings
     trips = self.trips
     completed_trips = trips.select {|t| !t.trip.nil?}
     completed_trips.map do |t|
-     trip_cost =  (t - 1.65) * 0.80
+      trip_cost =  (t - 1.65) * 0.80
     end
     total_earning = completed_trips.sum {|t| t.cost} 
     return total_earning
@@ -13,15 +13,11 @@ class Driver < ApplicationRecord
   
   def avg_rating
     trips = self.trips
-   
-     count = 0
+    
+    count = 0
     completed_trips = trips.select {|t| !t.rating.nil?}
     rating = completed_trips.sum {|t| t.cost} 
     avg_rating = rating / completed_trips.length
     return avg_rating
   end 
 end
-=======
-  has_many :trips
-end
->>>>>>> e758e92edfacb8a7e05a7e4f8d02b42f93d4e35c
