7a4726ba95513df24f2c746527a9e8e403569b28
Eve Le <nguyenthao.lee@gmail.com>
2019-10-08 22:44:47 -0700

    Add tests and view and implementation for passengers#index action

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -1,30 +1,49 @@
 require "test_helper"
 
 describe PassengersController do
-  describe "index" do
-    # Your tests go here
+  before do
+    @passenger = Passenger.create(name: "Hermione Granger", phone_num: "904-000-000")
   end
+  
+  describe "index" do
+    it "can get the list of passengers and repond with success" do
+      get passengers_path 
+      
+      must_respond_with :success
+    end
 
+    it "responds with success when there is no passengers saved" do
+      expect (Passenger.count).must_equal 1
+      
+      Passenger.destroy_all
+      expect (Passenger.count).must_equal 0
+
+      get passengers_path 
+      
+      must_respond_with :success
+    end
+  end
+  
   describe "show" do
     # Your tests go here
   end
-
+  
   describe "new" do
     # Your tests go here
   end
-
+  
   describe "create" do
     # Your tests go here
   end
-
+  
   describe "edit" do
     # Your tests go here
   end
-
+  
   describe "update" do
     # Your tests go here
   end
-
+  
   describe "destroy" do
     # Your tests go here
   end
