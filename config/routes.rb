74e3488afdfadc491f227c3f2b6c61d0eb48575d
Paige Davenport <davenport.paige@outlook.com>
2019-10-08 20:34:12 -0700

    Changed routes so only Passengers has nested trip routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :drivers do
-    resources :trips, only: [:index, :new]
-  end
+  resources :drivers 
 
   resources :passengers do 
     resources :trips, only: [:index, :new]
