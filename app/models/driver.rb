6d80dd2bbfd049321266300e15b5a4f124c34a02
Paige Davenport <davenport.paige@outlook.com>
2019-10-09 13:53:22 -0700

    Made updates to tests and schema from class discussion.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,6 +1,6 @@
 class Driver < ApplicationRecord
     has_many :trips
 
-    validates :name, presence: true, format: { with: /\A[a-z\sA-Z]+\z/, message: "only allows letters" }
-    validates :vin, presence: true, uniqueness: true
+    validates :name, presence: true
+    validates :vin, presence: true
 end
