3a8cff43318b41ff737a1cd4d92ed4ba4ff320bd
Farah Davoodi <farahdavoodi@gmail.com>
2019-10-09 14:48:32 -0700

    updated driver schema to remove car make and model and add active

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_234218) do
+ActiveRecord::Schema.define(version: 2019_10_09_214725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,10 +18,9 @@ ActiveRecord::Schema.define(version: 2019_10_07_234218) do
   create_table "drivers", force: :cascade do |t|
     t.string "name"
     t.string "vin"
-    t.string "car_make"
-    t.string "car_model"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "active"
   end
 
   create_table "passengers", force: :cascade do |t|
