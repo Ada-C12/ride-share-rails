b198903bc3762d9e73e8f73633027e66f222ad46
dora1405 <li.dora05@gmail.com>
2019-10-09 14:07:12 -0700

    Add trips controller CRUD and views

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -11,5 +11,71 @@ class TripsController < ApplicationController
       return
     end
   end
+  def new
+    @trip = trip.new
+  end
 
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      redirect_to trip_path(@trip.id)
+    else
+      render new_trip_path
+    end
+  end
+
+  def edit
+    @trip = Trip.find_by(id: params[:id])
+    if !@trip
+      redirect_to trips_path
+    end
+  end
+
+  def update
+    @trip = Trip.find_by(id: params[:id])
+    if !@trip
+      redirect_to trips_path
+      return
+    end
+    if @trip.update(trip_params)
+      redirect_to trip_path(@trip.id)
+    else
+      render new_trip_path
+    end
+  end
+
+  def destroy
+    trip_to_delete = Trip.find_by(id: params[:id])
+    if trip_to_delete.nil?
+      redirect_to trips_path
+      return
+    else
+      trip_to_delete.destroy
+      redirect_to trips_path
+      return
+    end
+  end
+
+  # def completed
+  #   completed_task = Task.find_by(id: params[:id])
+  #   if completed_task.nil?
+  #     redirect_to task_path
+  #     return
+  #   end
+  #   if completed_task.completed == nil
+  #     completed_task.completed = Date.today
+  #     completed_task.save
+  #     redirect_to tasks_path
+  #   else
+  #     completed_task.completed = nil
+  #     completed_task.save
+  #     redirect_to tasks_path
+  #   end
+  # end
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:date, :driver_id, :passenger_id, :cost, :rating)
+  end
 end
