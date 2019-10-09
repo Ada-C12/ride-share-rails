0cb09330e12cf0a8dd289509a82fdba8f250b68d
m.dove@live.com <m.dove@live.com>
2019-10-09 13:49:52 -0700

    added links to trip show page from passenger show and driver show

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,17 @@ class Passenger < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def count_rides
+    return self.trips.count
+  end
+
+  def total_charges
+    trip_array = self.trips
+    charge_sum = 0
+    trip_array.each do |trip|
+      charge_sum += trip[:cost]
+    end
+    return charge_sum
+  end
 end
