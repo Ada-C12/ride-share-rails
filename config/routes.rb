3002546d6a8b6f37d4d7b7ed1b95c50b7a649868
Sara Shah Baig <sarashahbaig@gmail.com>
2019-10-09 11:41:08 -0700

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,14 @@ Rails.application.routes.draw do
     resources :trips, only: [:index, :new]
   end
 
-  resources :drivers, only: [:index, :show, :new, :create, :edit] do
+  resources :drivers do #only: [:index, :show, :new, :create, :edit] do
     resources :trips, only: [:index, :new, :create]
   end
 
   
-  
   resources :homepages, only: [:index]
   resources :trips
+
+
+
 end
