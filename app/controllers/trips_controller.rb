f4d6018eeb90cf7e1169f69104a5723c42915a97
Farah Davoodi <farahdavoodi@gmail.com>
2019-10-09 15:54:58 -0700

    made updates to trip controller and views and routes

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,26 @@
 class TripsController < ApplicationController
+  
+  def index
+    driver_id = params[:driver_id]
+    passenger_id = params[passenger_id]
+    
+    if driver_id.nil? && passenger_id.nil?
+      @trips = Trip.all
+    elsif driver_id.nil?
+      @passenger = Passenger.find_by(id: passenger_id)
+      if @passenger
+        @trips = @passenger.trips
+      else
+        head :not_found
+      end
+    else
+      @driver = Driver.find_by(id: driver_id)
+      if @driver
+        @trips = @driver.trips
+      else
+        head :not_found
+      end
+    end
+  end
+  
 end
