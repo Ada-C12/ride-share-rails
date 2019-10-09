a3f0b2fe18006f5f4c89a385d71ba3b198438dda
Brianna Kemp <kemp.bri@gmail.com>
2019-10-08 17:18:35 -0700

    Added nav, added driver earnings method

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -8,5 +8,13 @@ class Driver < ApplicationRecord
     return Driver.all.order(name: :asc)
   end
   
+  def earnings
+    earnings = self.trips.map {|t| (t.cost - 165) * 0.8 }
+    return earnings.sum
+  end
   
+  def avg_rating
+    avg_rating = self.trips.map {|t| t.rating }
+    return avg_rating.sum / avg_rating.count
+  end
 end
