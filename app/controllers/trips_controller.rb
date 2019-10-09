921307f54f41eb19826dfcbfeb49f1932fa08a0d
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-10-08 22:00:58 -0700

    trip show has minimum functionality

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,9 @@
 class TripsController < ApplicationController
+ 
+ def show
+  @trip = Trip.find_by(id: params[:id])
+  
+  head :not_found if @trip.nil?
+ end
+ 
 end
--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -2,21 +2,40 @@ require "test_helper"
 
 describe TripsController do
   describe "show" do
-    # Your tests go here
+    before do
+      @test_trip = Trip.create
+    end
+    
+    it "shows accurate information about a trip" do
+      
+      get trips_path(Trip.find_by(id: @test_trip.id))
+      
+      must_respond_with :success
+      
+    end
+    
+    it "redirects to 404 not found if asked to show information about a trip with an invalid ID" do
+      
+      get trip_path(-1)
+      
+      must_respond_with :not_found
+      
+    end
+    
   end
-
+  
   describe "create" do
     # Your tests go here
   end
-
+  
   describe "edit" do
     # Your tests go here
   end
-
+  
   describe "update" do
     # Your tests go here
   end
-
+  
   describe "destroy" do
     # Your tests go here
   end
