7cf5e86a74b6eadfde63d6e49b626286805b8e86
Paige Davenport <davenport.paige@outlook.com>
2019-10-08 20:05:14 -0700

    Added Trips show action, tests, and view.

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -1,12 +1,47 @@
 require "test_helper"
 
 describe TripsController do
+  before do
+    passenger = Passenger.create(
+      name: "Jane",
+      phone_num: "8675309"
+    )
+
+    driver = Driver.create(
+      name: "Sarah",
+      vin: "848485859",
+      car_make: "Ford",
+      car_model: "Escape",
+      active: true
+    )
+
+    @trip = Trip.create(
+      date: "10-09-2019",
+      rating: 3,
+      cost: 20.40,
+      passenger_id: passenger.id,
+      driver_id: driver.id
+    )
+  end
+
   describe "show" do
-    # Your tests go here
+    it "responds with success when showing an existing valid trip" do
+      trip_id = @trip.id
+
+      get trip_path(trip_id)
+
+      must_respond_with :success
+    end
+
+    it "responds with a 404 with an invalid trip id" do
+      get trip_path(-1)
+
+      must_respond_with :not_found
+    end
   end
 
   describe "create" do
-    # Your tests go here
+
   end
 
   describe "edit" do
