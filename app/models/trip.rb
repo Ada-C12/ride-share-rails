d572e6349700a495b93cd152ff351b7aa5222d9c
Diana Han <dianah46@gmail.com>
2019-10-10 15:49:24 -0700

    added price as random value

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,5 @@ class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
   attribute :date, :datetime, default: -> {Time.now}
+  attribute :price, :float, default: -> {(1..100).rand}
 end
