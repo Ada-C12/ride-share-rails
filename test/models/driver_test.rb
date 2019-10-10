8e62a28c26ef47f4b30f5cea116dc0155e979278
gracexinran <gracexinran@gmail.com>
2019-10-10 15:02:59 -0700

    driver model test finshed

--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -15,7 +15,6 @@ describe Driver do
     new_driver.save
     driver = Driver.first
     [:name, :vin, :active, :car_make, :car_model].each do |field|
-
       # Assert
       expect(driver).must_respond_to field
     end
@@ -59,20 +58,82 @@ describe Driver do
 
   # Tests for methods you create should go here
   describe "custom methods" do
+    let (:driver) {
+      Driver.create(name: "Driver", vin: "123", active: nil)
+    }
+
+    let(:passenger) {
+      Passenger.create(name: "Haha Me", phone_num: "Nonono")
+    }
+
+    let(:trip_1) {
+      Trip.create(passenger_id: passenger.id, driver_id: driver.id, date: Date.today, cost: "1000", rating: "5")
+    }
+
+    let(:trip_2) {
+      Trip.create(passenger_id: passenger.id, driver_id: driver.id, date: Date.today, cost: "2000", rating: "4")
+    }
+
     describe "average rating" do
       # Your code here
+      it "can calculate the average rating of this driver when the driver made at least one trip" do
+        average_rating = ((trip_1.rating.to_i + trip_2.rating.to_i).to_f / 2).round(2)
+        driver.save
+        passenger.save
+
+        expect(driver.average_rating).must_equal average_rating
+      end
+
+      it "will return 0 if the driver did not take any trip" do
+        driver.save
+        passenger.save
+        Trip.destroy_all
+
+        expect(driver.average_rating).must_equal 0
+      end
     end
 
     describe "total earnings" do
       # Your code here
+      it "can calculate the total earning of this driver when the driver made at least one trip" do
+        total_earning = ((trip_1.cost.to_i - 1.65) * 0.8 + (trip_2.cost.to_i - 1.65) * 0.8).round(2)
+        driver.save
+        passenger.save
+
+        expect(driver.total_earning).must_equal total_earning
+      end
+
+      it "will return 0 if the driver did not take any trip" do
+        driver.save
+        passenger.save
+        Trip.destroy_all
+
+        expect(driver.total_earning).must_equal 0
+      end
     end
 
     describe "can go online" do
       # Your code here
+      it "can switch the active status of driver to nil which means the driver is available to accept a trip" do
+        driver.update(active: true)
+        expect(driver.active).must_equal true
+        driver.go_online
+
+        find_driver = Driver.find_by(id: driver.id)
+        expect(find_driver.active).must_be_nil
+      end
     end
 
     describe "can go offline" do
       # Your code here
+      it "can switch the active status of driver to true which means the driver is not available to accept a trip" do
+        driver.save
+        expect(driver.active).must_be_nil
+        driver.go_offline
+
+        find_driver = Driver.find_by(id: driver.id)
+        expect(find_driver.active).must_equal true
+      end
     end
 
     # You may have additional methods to test
