cd32509f38463caaa6ad904eeb0407ee3c4cfd30
Farah Davoodi <farahdavoodi@gmail.com>
2019-10-10 16:24:22 -0700

    Added total amount charged per passenger on model and #show page

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,16 @@ class Passenger < ApplicationRecord
 
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def total_charged
+    trips = self.trips
+    total = 0
+    trips.each do |trip|
+      if trip.cost != nil
+        total += trip.cost
+      end
+    end
+    return total/100
+  end
+
 end
