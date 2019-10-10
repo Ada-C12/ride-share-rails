5163fd526bab365462ae91cbe955480ee3589176
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-09 22:34:32 -0700

    now cost shows up as .cc format, yay

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,14 @@
 module ApplicationHelper
+  
   def display_date(date_obj)
     return date_obj.strftime("%b %d, %Y")
     
     # In case we want to add time into the display
     # return date_obj.strftime("%b %d, %Y, %l:%M %p")
+  end
+  
+  def usd(cents_float)
+    # cents is coming from database as a float...
+    return format("$%.2f", cents_float/100)
   end
 end
