a4c0a7b0f13d6557a3dcd073e2e8580e804954d0
Janice Huang <janicehuang@gmail.com>
2019-10-09 20:49:17 -0700

    add driver trip rating method, get working, fix trip list, add nested route-not in use

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,4 +2,13 @@ class Driver < ApplicationRecord
   has_many :trips, dependent: :nullify
   validates :name, presence: true
   validates :vin, presence: true
+
+
+  def avg_rating
+    sum = 0
+    self.trips.each do |trip|
+      sum += trip.rating.to_f
+    end
+    return sum / self.trips.count
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   resources :passengers, except: [:update]
   patch '/passengers/:id', to: 'passengers#update'
-  
-  resources :drivers, except: [:update]
+
+  resources :drivers, except: [:update] do
+    resources :trips, only: [:index]
+  end
   patch '/drivers/:id', to: 'drivers#update'
-  
+
   resources :trips, except: [:update, :index, :new]
   patch '/trips/:id', to: 'trips#update'
   get '/trips/:id/assign_rating/edit', to: 'trips#assign_rating_edit', as: 'assign_rating_edit'
-  
+
   root 'homepages#index'
   get '/homepages', to: 'homepages#index', as: 'homepages'
 end
