d93653a1ba574f8867217c602047ced9d2d7b15d
Samantha Coll <samcoll@Samanthas-MBP.localdomain>
2019-10-10 16:31:56 -0700

    created a method to create trips on trip_controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,19 @@
 class TripsController < ApplicationController
+  
+  def create
+    trip = Trip.create(
+      date: Date.today,
+      passenger_id: params[:id],
+      driver_id: Driver.find_a_driver,
+      cost: 5
+    )
+    
+    if trip.id
+      redirect_to root_path
+    else
+      head :server_error
+    end
+    
+  end
+  
 end
