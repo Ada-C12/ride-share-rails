f5b9e1fd3f8711c5977ef204839ee1be8c3cdf72
Dianna Tingg <dtingg@gmail.com>
2019-10-09 18:01:09 -0700

    added total money spent to passenger details page

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -4,5 +4,16 @@ class Passenger < ApplicationRecord
   validates :name, presence: true
   validates :phone_num, presence: true
   
-  
+  def total_money_spent
+    trips = self.trips
+    
+    if trips.nil?
+      return 0
+    else
+      total = trips.sum do |trip|
+        trip.cost
+      end
+      return total / 100.0
+    end
+  end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,16 +1,4 @@
 class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
-  
-  def self.total_money_spent
-    # trips = Trip.where(passenger_id: 3)
-    
-    # total = 0
-    
-    # trips.each do |trip|
-    #   total += trip.cost
-    #end
-    
-    return "blerg"
-  end
 end
