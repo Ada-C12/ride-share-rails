4749ce876562f78191d52e5ebd41afb75e0fa629
Diana Han <dianah46@gmail.com>
2019-10-10 15:52:49 -0700

    added active method to find out if driver is available

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,17 @@ class Driver < ApplicationRecord
   
   validates :name, presence: true
   validates :vin, presence: true
+
+  def active
+      # returns true is driver is on a trip
+      # returns false is driver is available
+    available_driver = self.trip.where.not(rating: nil)
+
+    if available_driver.length == 0
+      return false 
+    else
+      true
+    end
+
+  end
 end
