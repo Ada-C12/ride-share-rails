4a896b86bf359ecd93ceff1a58e498e024476953
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-10-09 20:45:46 -0700

    create for passenger

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,10 +2,30 @@ class PassengersController < ApplicationController
   def index
     @passengers = Passenger.all
   end
-
+  
   def show
     @passenger = Passenger.find_by(id: params[:id])
-
     head :not_found if @passenger.nil?
+  end
+  
+  def new
+    @passenger = Passenger.new
+  end
+  
+  def create
+    @passenger = Passenger.create(passenger_params)
+    if @passenger.save
+      redirect_to passenger_path(@passenger.id)
+    else
+      render new_passenger_path
+    end
+  end
+  
+  
+  
+  private
+  def passenger_params
+    params.require(:passenger).permit(:name, :phone_num)
+    
   end
 end
--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -17,7 +17,7 @@ describe PassengersController do
       
     end
   end
-
+  
   describe "show" do
     before do
       @test_passenger = Passenger.create
@@ -39,24 +39,43 @@ describe PassengersController do
       
     end
   end
-
+  
   describe "new" do
-    # Your tests go here
+    it "can get the new passenger path" do
+      get new_passenger_path
+      must_respond_with :success
+    end
   end
-
+  
   describe "create" do
-    # Your tests go here
+    
+    it "can can create a new instance of passenger " do
+      passenger_hash = {
+        passenger: {
+          name: "test_passenger",
+          phone_num: "1-888-975-5309",
+        },
+      }
+      
+      expect {
+        post passengers_path, params: passenger_hash }.must_change "Passenger.count", 1
+        
+        new_passenger = Passenger.find_by(name: passenger_hash[:passenger][:name])
+        
+      end
+    end
+    
   end
-
+  
   describe "edit" do
     # Your tests go here
   end
-
+  
   describe "update" do
     # Your tests go here
   end
-
+  
   describe "destroy" do
     # Your tests go here
   end
-end
+  # end
