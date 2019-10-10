65740183db90ec85c1fe34152e3871bb7f973379
Nicky Choi <nickyjinchoi@gmail.com>
2019-10-10 13:00:15 -0700

    Revised average rating method for Driver model and added some tests for TripsController

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -1,11 +1,23 @@
 require "test_helper"
 
 describe TripsController do
+  before do
+    @passenger = Passenger.create name: "A Passenger", phone_num: "206-123-3333"
+    @available_driver = Driver.create name: "Random Driver", vin: "XFHJKDHSLFKJDKL", available: true
+  end
   describe "show" do
     it "must respond with success when showing a specific trip by its ID" do
-      trip = Trip.create date: Date.today, rating: 5, cost: 2300, driver_id: 1, passenger_id: 5
+      trip_parameters = Trip.create_new_trip
+      trip = Trip.new(
+        passenger_id: @passenger.id,
+        date: trip_parameters[:date],
+        cost: trip_parameters[:cost],
+        driver_id: @available_driver.id,
+      )
 
-      get trip_path(trip.id)
+      trip.save
+
+      get trips_path(trip.id)
 
       must_respond_with :success
     end
@@ -20,62 +32,12 @@ describe TripsController do
   end
 
   describe "create" do
-    it "can create a new trip with valid information and redirects" do
-      trip_parameters = Trip.create_new_trip
-      trip_hash = {
-        trip: {
-          passenger_id: params[:passenger_id],
-          date: trip_parameters[:date],
-          cost: trip_parameters[:cost],
-          driver_id: trip_parameters[:driver_id],
-        },
-      }
-
-      expect {
-        post passenger_trips_path, params: trip_hash
-      }.must_change "Trip.count", 1
-    end
   end
 
   describe "edit" do
-    before do
-      @trip = Trip.create date: Date.today, rating: 5, cost: 2300, driver_id: 1, passenger_id: 5
-    end
-    it "responds with success when getting the edit page of an existing trip" do
-      get edit_trip_path(@trip.id)
-
-      must_respond_with :success
-    end
-
-    it "responds with redirect when getting the edit page for a non-existing trip" do
-      get edit_trip_path(-1)
-
-      must_respond_with :redirect
-      must_redirect_to passenger_path(@trip.passenger.id)
-    end
   end
 
   describe "update" do
-    it "can update an existing trip with valid information accurately, and redirect" do
-      existing_trip = Trip.create date: Date.today, rating: 3, cost: 1240, driver_id: 8, passenger_id: 15
-
-      updated_trip_hash = {
-        trip: {
-          rating: 5,
-        },
-      }
-
-      expect {
-        patch trip_path(existing_trip.id), params: updated_trip_hash
-      }.wont_change "Trip.count"
-
-      updated_trip = Trip.find_by(id: existing_trip.id)
-
-      expect(updated_trip.rating).must_equal updated_trip_hash[:trip][:rating]
-
-      must_respond_with :redirect
-      must_redirect_to passenger_path(updated_trip.passenger.id)
-    end
   end
 
   describe "destroy" do
