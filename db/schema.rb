a911ca341c30805609a163ca6ad85182e3d7559a
Mira <calopter@gmail.com>
2019-10-09 18:37:25 -0700

    adds active column to drivers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_204521) do
+ActiveRecord::Schema.define(version: 2019_10_10_013515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_10_08_204521) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "active"
   end
 
   create_table "passengers", force: :cascade do |t|
