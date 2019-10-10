e954e0ed6fff3feb9fb225530034cb061707de7c
Kristina <kristina.tanya@gmail.com>
2019-10-10 15:52:21 -0700

    Tests in progress for drivers controller

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -2,13 +2,30 @@ require "test_helper"
 
 describe DriversController do
   describe "index" do
-    it "can get index" do
-      # Your code here
+    it "gives back a successful response" do
+      get drivers_path
+
+      must_respond_with :success
+    end
+
+    it "can get the root path" do
+      get root_path
+
+      must_respond_with :success
     end
   end
 
   describe "show" do
-    # Your tests go here
+    it "gives back a successful response" do
+      valid_driver = Driver.create(name: "James Franco", vin:"425449888")
+      get driver_path(valid_driver.id)
+      must_respond_with :success
+    end
+
+    it "gives back a 404 response if they try an invalid id" do
+      get driver_path("100000")
+      must_respond_with :not_found
+    end
   end
 
   describe "edit" do
