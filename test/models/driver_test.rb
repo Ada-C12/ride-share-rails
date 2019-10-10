41a3b6b2927712cbbd85d6d676453761de195b64
m.dove@live.com <m.dove@live.com>
2019-10-10 14:14:25 -0700

    Began writing tests for custom methods for Driver Model

--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -60,15 +60,50 @@ describe Driver do
   # Tests for methods you create should go here
   describe "custom methods" do
     describe "average rating" do
-      # Your code here
+      it "returns nil if the driver has no rides" do
+        new_driver.update(active: false)
+        expect(new_driver.average_rating).must_be_nil
+      end
+      it "can return the average rating of a driver's trips in the form of a float" do
+        new_driver.update(active: false)
+        passenger = Passenger.create(name: "Mac Do", phone_num: "253.394.8901")
+        trip = Trip.create(driver_id: new_driver.id, passenger_id: passenger.id, rating: 5, cost: 1000.00, date: Date.new(2019, 3, 4))
+        passenger2 = Passenger.create(name: "Do li", phone_num: "253.867.5309")
+        trip2 = Trip.create(driver_id: new_driver.id, passenger_id: passenger2.id, rating: 4, cost: 1000.00, date: Date.new(2019, 4, 4))
+
+        expect(new_driver.average_rating).must_equal 4.5
+        expect(new_driver.average_rating).must_be_instance_of Float
+      end
     end
 
     describe "total earnings" do
-      # Your code here
+      it "must return the total cost of the driver's trips" do
+        new_driver.update(active: false)
+        passenger = Passenger.create(name: "Mac Do", phone_num: "253.394.8901")
+        trip = Trip.create(driver_id: new_driver.id, passenger_id: passenger.id, rating: 5, cost: 1000.00, date: Date.new(2019, 3, 4))
+        passenger2 = Passenger.create(name: "Do li", phone_num: "253.867.5309")
+        trip2 = Trip.create(driver_id: new_driver.id, passenger_id: passenger2.id, rating: 4, cost: 1000.00, date: Date.new(2019, 4, 4))
+
+        expect(new_driver.total_earnings).must_equal 1597.36
+      end
+      it "must return a float" do
+        new_driver.update(active: false)
+        passenger = Passenger.create(name: "Mac Do", phone_num: "253.394.8901")
+        trip = Trip.create(driver_id: new_driver.id, passenger_id: passenger.id, rating: 5, cost: 1000.00, date: Date.new(2019, 3, 4))
+        passenger2 = Passenger.create(name: "Do li", phone_num: "253.867.5309")
+        trip2 = Trip.create(driver_id: new_driver.id, passenger_id: passenger2.id, rating: 4, cost: 1000.00, date: Date.new(2019, 4, 4))
+
+        expect(new_driver.total_earnings).must_be_instance_of Float
+      end
     end
 
-    describe "can go online" do
-      # Your code here
+    describe "toggle_active" do
+      it "can toggle the active status of a driver when a ride is requested" do
+        new_driver.update(active: false)
+        passenger = Passenger.create(name: "Mac Do", phone_num: "253.394.8901")
+        trip = Trip.create(driver_id: new_driver.id, passenger_id: passenger.id, rating: 5, cost: 1000.00, date: Date.new(2019, 3, 4))
+        expect(new_driver.toggle_active).must_equal true
+      end
     end
 
     describe "can go offline" do
