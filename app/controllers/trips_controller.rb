c183099cb86394b16d94a4aa409767e4dc37881b
Diana Han <dianah46@gmail.com>
2019-10-09 21:08:51 -0700

    able to route to new trip form from a passenger show page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -23,4 +23,31 @@ class TripsController < ApplicationController
     end
   end
   
+  def new
+    passenger_id = params[:passenger_id]
+    @trip = Trip.new
+    if passenger_id.nil?
+      @passengers = Passenger.all
+    else
+      @passengers = [Passenger.find_by(id: passenger_id)]
+    end
+  end
+
+  def create 
+    # will not set params for price, rating. by leaving it blank, it'll default to nil
+
+    @trip = Trip.new( driver_id: driver_id, passenger_id: passenger_id, date: Time.now )
+
+    if @trip.save
+      redirect_to trip_path(@trip.id)
+    else
+      
+      render new_trip_path
+    end
+  end
+
+  private
+  def trip_params
+    return params.require(:trip).permit(:date, :cost, :rating, :driver_id, :passenger_id)
+  end
 end
