86e741a62bc8c2c418eeb96d33d3371fd0be9c31
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-10-10 15:05:30 -0700

    Generated two migrations to delete make and model from drivers schema.

---
+++ db/migrate/20191010220413_remove_model_from_drivers.rb
@@ -0,0 +1,5 @@
+class RemoveModelFromDrivers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :drivers, :car_model
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_014824) do
+ActiveRecord::Schema.define(version: 2019_10_10_220413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,6 @@ ActiveRecord::Schema.define(version: 2019_10_10_014824) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "vin"
-    t.string "car_make"
-    t.string "car_model"
     t.boolean "available", default: true
   end
 
