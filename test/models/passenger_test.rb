806a3987492b75129f31b96e4adc7ca813fc7f22
Linnea Johnson <linnea.johnson1@gmail.com>
2019-10-11 14:42:31 -0700

    Added tests to Trips test for model

--- test/models/passenger_test.rb
+++ test/models/passenger_test.rb
@@ -42,6 +42,5 @@ describe Passenger do
     describe "total_money_spent" do
       # Your code here
     end
-    # You may have additional methods to test here
   end
 end
--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -1,24 +1,44 @@
 require "test_helper"
 
 describe Trip do
+  let (:passenger_test) {
+    Passenger.create(name: "Test Passenger!", phone_num: "555-555-5555")
+  }
+  let (:driver_test) {
+    Driver.create(name: "Test Driver!", vin: "FSD34534SLDK", available: true)
+  }
+  let (:new_trip) {
+    Trip.create(date: Date.today, rating: 5, cost: 1050, passenger_id: passenger_test.id, driver_id: driver_test.id)
+  }
+
   it "can be instantiated" do
-    # Your code here
+    expect(new_trip.valid?).must_equal true
   end
 
   it "will have the required fields" do
-    # Your code here
+    new_trip.save
+    trip = Trip.first
+    [:date, :rating, :cost, :passenger_id, :driver_id].each do |field|
+
+    # Assert
+      expect(trip).must_respond_to field
+    end
   end
 
   describe "relationships" do
-    # Your tests go here
-  end
+    it "can has a passenger id and driver id" do
+      new_trip.save
 
-  describe "validations" do
-    # Your tests go here
-  end
+      new_driver = Driver.create(name: "Waldo", vin: "ALWSS52P9NEYLVDE9")
+      new_passenger = 
+      trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5, cost: 1234)
+      trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3, cost: 6334)
 
-  # Tests for methods you create should go here
-  describe "custom methods" do
-    # Your tests here
+      # Assert
+      expect(new_passenger.trips.count).must_equal 2
+      new_passenger.trips.each do |trip|
+        expect(trip).must_be_instance_of Trip
+      end
+    end
   end
 end
