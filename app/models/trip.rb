8eb6d0a7a73281c8dd14ca61fd3eb40cfada556f
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-10-11 16:28:45 -0700

    Added business logic for driver total earnings. Changed cost format to dollars and cents and formatted so that two decimals always show, i.e. if float is 2.5, the number displayed is 2.50.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,11 +5,12 @@ class Trip < ApplicationRecord
   belongs_to :passenger
   
   def self.trips_total(passenger_id)
-    sum = 0
+    subtotal = 0
     Trip.where(passenger_id: passenger_id).each do |trip|
-      sum += trip.cost
+      subtotal += trip.cost
     end
-    return sum
+    total_price_trips = '%.2f' % (subtotal / 100).round(2)
+    return total_price_trips
   end
   
   def self.driver_avg_rating(driver_id)
@@ -23,5 +24,22 @@ class Trip < ApplicationRecord
     average_rating = ratings.sum.to_f / ratings.length
     return average_rating.round(2)
   end
+  
+  def self.driver_total_earnings(driver_id)
+    # Driver gets 80 percent of trip cost after fee of $1.65 is collected
+    earnings = []
+    Trip.where(driver_id: driver_id).each do |trip|
+      earnings << (trip.cost - 165) * 0.8
+    end
+    total_earnings = '%.2f' % (earnings.sum / 100).round(2)
+    return total_earnings
+  end
+
+# trip.cost.cents_to_dollars
+
+#   def cents_to_dollars(penny_amount)
+#     .to_f / 100
+#     return amount_in_dollars
+#   end
   
 end
