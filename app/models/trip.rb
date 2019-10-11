8f1a39818869b7cae511ad03de324dcfbd41ff22
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-10-10 21:17:11 -0700

    updated show for driver and passenger to reflect price/cost as money

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,17 @@
 class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
-
+  
+  def converter
+    cost = @trip.cost
+    @converted_cost = converter(cost)
+  end
+  
+  def converter(integer)
+    integer = integer.to_s
+    cents = integer[-2..-1]
+    dollars = integer[0..-3]
+    return_statement = "#{dollars}.#{cents}"
+  end
   
 end
