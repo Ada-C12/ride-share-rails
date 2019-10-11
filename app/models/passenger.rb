9aab61f069c8fa5dc4da5849e503dcfaa468149f
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-10-11 12:03:20 -0700

    Added Regex validations for passenger name and phone

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,10 +1,10 @@
 class Passenger < ApplicationRecord
   has_many :trips
-
+  
   def self.alpha_passengers
     return Passenger.order(name: :asc)
   end
-
-  validates :name, presence: true
-  validates :phone_number, presence: true
+  
+  validates :name, presence: true, format: {with /\S+\s{1}/}
+  validates :phone_number, presence: true, format: {with /\S+\s{1}/}
 end
