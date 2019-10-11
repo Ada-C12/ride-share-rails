88ba5d24edff058ca678b8940303287446b73e40
Vi <vanessareigosa@gmail.com>
2019-10-11 14:59:53 -0700

    fixed drivercontroller test errors

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -2,16 +2,38 @@ require "test_helper"
 
 describe TripsController do
   #Does each trip need me to create a new instance of driver and passenger?
+  let (:passenger) {
+    Passenger.create name: "Mr. Jared", phone_num: "954-666-6666"
+  }
+
+  let (:driver) {
+    Driver.create name: "Mx. Dee", vin: "56R7FGYHFS"
+  }
+
   let (:trip) {
-   Trip.create driver_id: , passenger_id: , date: Date.today}
+   Trip.create driver_id: driver.id, passenger_id: passenger.id, date: Date.today}
 
   describe "show" do
-    get trip_path
-    must_respond_with :success
+    it "shows a list of all a passengers trip" do 
+      get passenger_trip_path
+      must_respond_with :success
+    end 
   end
 
   describe "create" do
-    # Your tests go here
+    it "successfully creates a new instance of a trip for a passenger" do
+      
+      trip_hash = {
+        trip: {
+          driver_id: driver_id,
+          passenger_id: passenger_id,
+          date: Date.today,
+        }
+      }
+      
+      expect {post new_passenger_trip_path params: trip_hash}.must_change "Passenger.trips.count", 1
+
+    end 
   end
 
   describe "edit" do
