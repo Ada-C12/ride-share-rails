888bfc9477b629233e108c485dc9a1c87246cb9f
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-10-11 08:49:16 -0700

    Fixed trip-create test

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -18,36 +18,40 @@ describe TripsController do
   #To-do test to create with passenger
   describe "create" do
     it "can create a new trip" do
+      passenger = Passenger.create(name: "Kelsey", phone_num: "222-222-2222")
+      driver = Driver.create(name: "Brianna", vin: "1234567890")
       trip_hash = {
-        trip: {
-          date: "2019-10-5",
-          rating: 2,
-          cost: 4
-        }
-      }
-      
-      expect {
-        post trips_path, params: trip_hash
-      }.must_change "Trip.count", 1
-      
-      new_trip = Trip.find_by(name: trip_hash[:trip][:date])
-      expect(new_trip.rating).must_equal trip_hash[:trip][:rating]
-      expect(new_trip.cost).must_equal trip_hash[:trip][:cost]
-      
-      must_respond_with :redirect
-      must_redirect_to trip_path(new_trip.id)
-    end
-  end
+      trip: {
+      date: "2019-10-5",
+      rating: 2,
+      cost: 4,
+      passenger_id: passenger.id,
+      driver_id: driver.id
+    }
+  }
   
-  describe "edit" do
-    # Your tests go here
-  end
-  
-  describe "update" do
-    # Your tests go here
-  end
-  
-  describe "destroy" do
-    # Your tests go here
-  end
+  expect {
+  post trips_path, params: trip_hash
+}.must_change "Trip.count", 1
+
+new_trip = Trip.find_by(date: trip_hash[:trip][:date])
+expect(new_trip.rating).must_equal trip_hash[:trip][:rating]
+expect(new_trip.cost).must_equal trip_hash[:trip][:cost]
+
+must_respond_with :redirect
+must_redirect_to trip_path(new_trip.id)
+end
+end
+
+describe "edit" do
+  # Your tests go here
+end
+
+describe "update" do
+  # Your tests go here
+end
+
+describe "destroy" do
+  # Your tests go here
+end
 end
