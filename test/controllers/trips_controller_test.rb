86c4cdac0a8f7f64e190ce689c0bf9e40abf07b2
Kristina <kristina.tanya@gmail.com>
2019-10-10 22:07:54 -0700

    trip controller test passing for update and destroy actions

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -10,7 +10,43 @@ describe TripsController do
   end
 
   describe "update" do
-    # Your tests go here
+    before do
+      @passenger = Passenger.create(name: "Georgina", phone_num: "111-111-1211")
+      @driver = Driver.create(name: "Lex", vin: "123", active: true, car_make: "Cherry", car_model: "DR5")
+      @existing_trip = Trip.create(driver_id: @driver.id, passenger_id: @passenger.id, date: Time.now, rating: 4, cost: 100)
+      @updated_trip_form_data = {
+        trip: {
+          rating: 1,
+          cost: 900,
+        }
+      }
+    end
+
+    it "can update an existing trip" do
+      patch trip_path(@existing_trip.id), params: @updated_trip_form_data
+
+      expect(Trip.find_by(id: @existing_trip.id).rating).must_equal 1
+      expect(Trip.find_by(id: @existing_trip.id).cost).must_equal 900
+    end
+
+    it "can't update an existing trip given the wrong parameters" do
+      wrong_trip_form_data = {
+        trip: {
+          ratinsg: 2,
+          costs: 89000,
+        }
+      }
+      patch trip_path(@existing_trip.id), params: wrong_trip_form_data
+
+      expect(Trip.find_by(id: @existing_trip.id).rating).must_equal 4
+      expect(Trip.find_by(id: @existing_trip.id).cost).must_equal 100
+    end
+
+    it "will respond with not_found if given an invalid id" do
+      patch trip_path(-1), params: @updated_trip_form_data
+      must_respond_with :not_found
+    end
+    
   end
 
   describe "destroy" do
