c1626b2501a307f42c839ad3ae0620aa517209ee
Morgan Schuler <morganlschuler@gmail.com>
2019-10-11 15:30:46 -0700

    Tried to fix model tests but struggled with a couple oops

--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -2,23 +2,36 @@ require "test_helper"
 
 describe Trip do
   it "can be instantiated" do
-    # Your code here
+    passenger = Passenger.create(name: "Emmanlle Breiterg", phone_num: "(707) 341-7157")
+    driver = Driver.create(name: "Dr. Ken Berge", vin: "SXMMLZX8XGDN7L7TM", available: true)
+    trip = Trip.create(driver_id: driver.id, passenger_id: passenger.id, date: Date.today, cost: 10.00)
+    
+    expect(trip.valid?).must_equal true
   end
-
+  
   it "will have the required fields" do
-    # Your code here
-  end
-
-  describe "relationships" do
-    # Your tests go here
-  end
-
-  describe "validations" do
-    # Your tests go here
-  end
-
-  # Tests for methods you create should go here
-  describe "custom methods" do
-    # Your tests here
+    passenger = Passenger.create(name: "Emmanlle Breiterg", phone_num: "(707) 341-7157")
+    driver = Driver.create(name: "Dr. Ken Berge", vin: "SXMMLZX8XGDN7L7TM", available: true)
+    trip = Trip.create(driver_id: driver.id, passenger_id: passenger.id, date: Date.today, cost: 10.00)
+    
+    trip.save
+    trip = Trip.first
+    [:passenger_id, :driver_id, :rating, :cost, :date].each do |field|
+      
+      # Assert
+      expect(trip).must_respond_to field
+    end
   end
 end
+
+# describe "relationships" do
+#   it "can have many trips" do
+
+#     end
+#   end
+# end
+
+
+# # Tests for methods you create should go here
+
+# end
