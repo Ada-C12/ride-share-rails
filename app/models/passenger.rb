af21dfd2cd3040efcfd000ec06b796e894bc8ddb
emaust <erikamaust@gmail.com>
2019-10-10 18:59:00 -0700

    Method created in passenger model for total amount charged

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,8 +1,9 @@
 class Passenger < ApplicationRecord
-
+  has_many :trips
+  
   validates :name, presence: true
   validates :phone_num, presence: true 
-
+  
   #see the total amount the passenger has been charged
   def request_trip
     date = Date.today
@@ -11,4 +12,11 @@ class Passenger < ApplicationRecord
     trip_params = {date: date, driver_id: driver_id , passenger_id: passenger_id}  
     return trip_params
   end
+  
+  def total_charged
+    total_trips = self.trips.where.not(cost: nil)
+    total_cost = total_trips.map { |trip| trip.cost }.sum
+    return total_cost * 0.01
+  end
+  
 end
