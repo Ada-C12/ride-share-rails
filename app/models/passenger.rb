f6cb1e63ccdb104f9ba096131ecf935cfe6e0ac8
Eve Le <nguyenthao.lee@gmail.com>
2019-10-10 23:21:41 -0700

    Update passenger view to show total expense in dollar

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,10 +1,11 @@
 class Passenger < ApplicationRecord
-    has_many :trips, dependent: :nullify
-
-    validates :name, presence: true
-    validates :phone_num, presence: true
-
-    def total_expense
-        return self.trips.map {|trip| trip.cost }.sum
-    end
+  has_many :trips, dependent: :nullify
+  
+  validates :name, presence: true
+  validates :phone_num, presence: true
+  
+  def total_expense_to_dollars
+    total_expense = self.trips.map {|trip| trip.cost }.sum
+    return total_expense / 100.0
+  end
 end
