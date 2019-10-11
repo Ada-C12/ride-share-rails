115a260ecd25db40ea6de3faf2736db3e8fcd0b2
Samantha Coll <samcoll@Samanthas-MacBook-Pro.local>
2019-10-10 20:47:40 -0700

    updated method total_amount_charged

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -4,12 +4,13 @@ class Passenger < ApplicationRecord
   validates :name, presence: true
   validates :phone_num, presence: true
   
+  # I need to update so the cost shows in $
   def total_amount_charged  
     sum = 0  
     self.trips.each do |trip|
       sum += trip.cost
     end
-    return sum
+    return sum/100.0
   end  
 end 
 
