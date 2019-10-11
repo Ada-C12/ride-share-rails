b1052ae8696017c72b577c86a01de53b9a8dc531
Alice <alicesunhi@gmail.com>
2019-10-11 14:16:45 -0700

    fixed render/redirect bug ty becca

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -35,114 +35,114 @@ class TripsController < ApplicationController
     cost = rand(1000..9999).to_i
     
     data_hash = {
-      date: date,
-      driver_id: driver.id,
-      passenger_id: params[:passenger_id],
-      rating: nil,
-      cost: cost,
-    }
-    
-    @trip = Trip.new(data_hash)
-    
-    if @trip.save
-      redirect_to passenger_path(@trip.passenger_id)
-      return
-    else
-      redirect_to root_path
-      return
-    end
+    date: date,
+    driver_id: driver.id,
+    passenger_id: params[:passenger_id],
+    rating: nil,
+    cost: cost,
+  }
+  
+  @trip = Trip.new(data_hash)
+  
+  if @trip.save
+    redirect_to passenger_path(@trip.passenger_id)
+    return
+  else
+    redirect_to root_path
+    return
+  end
+end
+
+def edit
+  @trip = Trip.find_by(id: params[:id])
+  
+  if @trip.nil?
+    redirect_to root_path
+    return
+  end
+end
+
+def update
+  @trip = Trip.find_by(id: params[:id])
+  
+  if @trip.nil?
+    head :not_found
+    return
   end
   
-  def edit
-    @trip = Trip.find_by(id: params[:id])
-    
-    if @trip.nil?
-      redirect_to root_path
-      return
-    end
-  end
-  
-  def update
-    @trip = Trip.find_by(id: params[:id])
-    
-    if @trip.nil?
-      head :not_found
-      return
-    end
-    
-    if params[:trip][:driver_name].blank? || params[:trip][:passenger_name].blank? || params[:trip][:cost].blank?
-      prev = Rails.application.routes.recognize_path(request.referrer)
-      if prev[:action] == "rate" || prev[:action] == "show"
-        @trip.rating = params[:trip][:rating]
-        if @trip.save
-          redirect_to trip_path(@trip.id)
-          return
-        else
-          flash.now[:notice] = "Unable to save rating."
-          render :rate
-          return
-        end
+  if params[:trip][:driver_name].blank? || params[:trip][:passenger_name].blank? || params[:trip][:cost].blank?
+    prev = Rails.application.routes.recognize_path(request.referrer)
+    if (prev[:action] == "rate" || prev[:action] == "show") && params[:action] != "edit" && prev[:action] != "edit"
+      @trip.rating = params[:trip][:rating]
+      if @trip.save
+        redirect_to trip_path(@trip.id)
+        return
       else
-        flash.now[:notice] = "You must enter a value for all fields."
-        render :edit
+        flash[:notice] = "Unable to save rating."
+        redirect_to rate_path(params[:id])
         return
       end
-    end 
-    
-    driver = Driver.find_by(name: params[:trip][:driver_name])
-    passenger = Passenger.find_by(name: params[:trip][:passenger_name])
-    
-    if driver.nil? 
-      flash.now[:notice] = "This driver is not in our system."
-      render :edit
-      return
-    end
-    
-    if passenger.nil?
-      flash.now[:notice] = "This passenger is not in our system."
-      render :edit
-      return
-    end
-    
-    @trip.driver_id = driver.id
-    @trip.passenger_id = passenger.id
-    
-    if @trip.update(trip_params)
-      redirect_to trip_path(@trip.id)
-      return
     else
-      render :edit
+      flash[:notice] = "You must enter a value for all fields."
+      redirect_to edit_trip_path(params[:id])
       return
     end
+  end 
+  
+  driver = Driver.find_by(name: params[:trip][:driver_name])
+  passenger = Passenger.find_by(name: params[:trip][:passenger_name])
+  
+  if driver.nil? 
+    flash[:notice] = "This driver is not in our system."
+    redirect_to edit_trip_path(params[:id])
+    return
   end
   
-  def destroy
-    trip_id = params[:id].to_i
-    @trip = Trip.find_by(id: trip_id)
-    
-    if @trip.nil?
-      head :not_found
-      return
-    else
-      @trip.destroy
-      redirect_to root_path
-      return
-    end
+  if passenger.nil?
+    flash[:notice] = "This passenger is not in our system."
+    redirect_to edit_trip_path(params[:id])
+    return
   end
   
-  def rate
-    @trip = Trip.find_by(id: params[:id])
-    
-    if @trip.nil?
-      redirect_to root_path
-      return
-    end
+  @trip.driver_id = driver.id
+  @trip.passenger_id = passenger.id
+  
+  if @trip.update(trip_params)
+    redirect_to trip_path(@trip.id)
+    return
+  else
+    redirect_to edit_trip_path(params[:id])
+    return
   end
+end
+
+def destroy
+  trip_id = params[:id].to_i
+  @trip = Trip.find_by(id: trip_id)
   
-  private
-  
-  def trip_params
-    return params.require(:trip).permit(:date, :rating, :cost, :driver_id, :passenger_id)
+  if @trip.nil?
+    head :not_found
+    return
+  else
+    @trip.destroy
+    redirect_to root_path
+    return
   end
+end
+
+def rate
+  @trip = Trip.find_by(id: params[:id])
   
+  if @trip.nil?
+    redirect_to root_path
+    return
+  end
+end
+
+private
+
+def trip_params
+  return params.require(:trip).permit(:date, :rating, :cost, :driver_id, :passenger_id)
+end
+
 end
