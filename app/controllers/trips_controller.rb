5560fd5eef407482339b9fc04d79726f6a61c276
Alice <alicesunhi@gmail.com>
2019-10-10 19:41:34 -0700

    added feature so that passenger can only request a trip if they dont have any unrated trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -10,7 +10,17 @@ class TripsController < ApplicationController
     end
   end
   
-  def create   
+  def create
+    passenger_trips = Trip.where(passenger_id: params[:passenger_id])
+    
+    passenger_trips.each do |trip|
+      if trip.rating == nil
+        flash[:notice] = "You may only request a trip after your previous one has completed and you rate it."
+        redirect_to passenger_path(params[:passenger_id])
+        return
+      end
+    end
+    
     driver = Driver.get_driver
     
     if driver == nil
