921660ae63c76a8134661d5d368dab8a05235322
Sara Shah Baig <sarashahbaig@gmail.com>
2019-10-11 13:49:25 -0700

    test code edited

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -1,201 +1,177 @@
-# require "test_helper"
+require "test_helper"
 
-# describe DriversController do
-#   # Note: If any of these tests have names that conflict with either the requirements or your team's decisions, feel empowered to change the test names. For example, if a given test name says "responds with 404" but your team's decision is to respond with redirect, please change the test name.
-#   let (:driver) {
-#     driver.create name: "Bernardo Prosacco",
-#                   vin: "WBWSS52P9NEYLVDE9"
-#   }
-#   describe "index" do
-#     it "responds with success when there are many drivers saved" do
-#       # Arrange
-#       # Ensure that there is at least one Driver saved
-#       get drivers_path
-#       # Act
-#       must_respond_with :success
-#       # Assert
-#     end
+describe DriversController do
+  # Note: If any of these tests have names that conflict with either the requirements or your team's decisions, feel empowered to change the test names. For example, if a given test name says "responds with 404" but your team's decision is to respond with redirect, please change the test name.
+  let (:driver) {
+    Driver.create name: "Bernardo Prosacco",
+                  vin: "WBWSS52P9NEYLVDE9"
+  }
+  describe "index" do
+    it "responds with success when there are many drivers saved" do
+      get drivers_path
+      must_respond_with :success
+      # Assert
+    end
 
-#     it "responds with success when there are no drivers saved" do
-#       # Arrange
-#       # Ensure that there are zero drivers saved
-#       get root_path
-#       # Act
-#       # Assert
-#       must_respond_with :success
-#     end
-#   end
+    it "responds with success when there are no drivers saved" do
+      # Arrange
+      get root_path
+      must_respond_with :success
+    end
+  end
 
-#   describe "show" do
-#     it "responds with success when showing an existing valid driver" do
-#       # Arrange
-#       # Ensure that there is a driver saved
-#       get driver_path(driver.id)
-#       # Act
-#       # Assert
-#       must_respond_with :success
-#     end
+  describe "show" do
+    it "responds with success when showing an existing valid driver" do
+      get driver_path(driver.id)
+      must_respond_with :success
+    end
 
-#     it "responds with 404 with an invalid driver id" do
-#       # Arrange
-#       # Ensure that there is an id that points to no driver
-#       get passenger_path(-1)
-#       # Act
-#       must_respond_with :redirect
-#       # Assert
+    it "responds with 404 with an invalid driver id" do
+      get driver_path(-1)
+      must_respond_with :redirect
+      # Assert
+    end
+  end
 
-#     end
-#   end
+  describe "new" do
+    it "responds with success, can get the new driver page" do
+      get new_driver_path
+      must_respond_with :success
+    end
+  end
 
-#   # Arrange
-#       # Ensure that there is an id that points to no driver
-#       # get passenger_path(-1)
-#       # # Act
-#       # must_respond_with :redirect
-#       # Assert
+  describe "create" do
+    it "can create a new driver with valid information accurately, and redirect" do
 
-#   describe "new" do
-#     it "responds with success, can get the new driver page" do
-#       # Act
-#       get new_passenger_path
-#       # Assert
-#       must_respond_with :success
-#     end
-#   end
+      driver_hash = {
+        driver: {
+          name: "Bernardo Prosacco",
+          vin: "WBWSS52P9NEYLVDE9"
+        },
+      }
+      
+      expect {
+        post drivers_path, params: driver_hash
+      }.must_change "Driver.count", 1
 
-#   describe "create" do
-#     it "can create a new driver with valid information accurately, and redirect" do
-#       # Arrange
-#       driver_hash = {
-#         driver: {
-#           name: "Bernardo Prosacco",
-#           vin: "WBWSS52P9NEYLVDE9"
-#         },
-#       }
-#       # Set up the form data
+      new_driver = Driver.find_by(name: driver_hash[:driver][:name])
+      expect(new_driver.vin).must_equal driver_hash[:driver][:vin]
 
-#       # Act-Assert
-#       # Ensure that there is a change of 1 in Driver.count
-#       expect {
-#         post drivers_path, params: driver_hash
-#       }.must_change "Driver.count", 1
-#       # Assert
-#       new_driver = Driver.find_by(name: driver_hash[:driver][:name])
-#       expect(new_driver.vin).must_equal driver_hash[:driver][:vin]
+      must_respond_with :redirect
+      must_redirect_to driver_path(new_driver.id)
 
-#       must_respond_with :redirect
-#       must_redirect_to driver_path(new_driver.id)
-#       # Find the newly created Driver, and check that all its attributes match what was given in the form data
-#       # Check that the controller redirected the user
-#     end
+    end
 
-#     it "does not create a driver if the form data violates Driver validations, and responds with a redirect" do
-#       # Note: This will not pass until ActiveRecord Validations lesson
-#       # Arrange
-#       invalid_id = -1
-#       # Set up the form data so that it violates Driver validations
-#       get edit_driver_path(invalid_id)
-#       # Act-Assert
-#       must_respond_with :redirect
-#       # Ensure that there is no change in Driver.count
-#       must_redirect_to driver_path
-#       # Assert
-#       # Check that the controller redirects
+    it "does not create a driver if the form data violates Driver validations, and responds with a redirect" do
 
-#     end
-#   end
+      invalid_id = -1
+      get edit_driver_path(invalid_id)
+      must_respond_with :redirect
+      must_redirect_to driver_path
+      # Assert
+      # Check that the controller redirects
+    end
+  end
   
-#   describe "edit" do
-#     it "responds with success when getting the edit page for an existing, valid driver" do
-#       # Arrange
-#       # Ensure there is an existing driver saved
+  describe "edit" do
+    it "responds with success when getting the edit page for an existing, valid driver" do
+      # Arrange
+      # Ensure there is an existing driver saved
+        edit_driver_path(driver.id)
+      # Act
+        must_respond_with :success
+      # Assert
 
-#       # Act
+    end
 
-#       # Assert
+    it "responds with redirect when getting the edit page for a non-existing driver" do
+      # Arrange
+      # Ensure there is an invalid id that points to no driver
+        invalid_id = -100
+      # Act
+        get edit_driver_path(invalid_id)
+      # Assert
+        must_redirect_to driver_path
+    end
+  end
 
-#     end
+  describe "update" do
+    it "can update an existing driver with valid information accurately, and redirect" do
+      # Arrange
+      # Ensure there is an existing driver saved
+      # Assign the existing driver's id to a local variable
+      # Set up the form data
+      input_driver = Driver.create(name: "Input driver", vin: "1111111111")
+      input_updates = {driver: {name: "Updated driver", vin: "2222222222"}}
 
-#     it "responds with redirect when getting the edit page for a non-existing driver" do
-#       # Arrange
-#       # Ensure there is an invalid id that points to no driver
+      patch driver_path(input_driver), params: input_updates
 
-#       # Act
+      input_driver.reload
+      expect(input_driver.name).must_equal "Updated driver"
 
-#       # Assert
+      # Act-Assert
+      # Ensure that there is no change in Driver.count
+      # Assert
+      # Use the local variable of an existing driver's id to find the driver again, and check that its attributes are updated
+      # Check that the controller redirected the user
 
-#     end
-#   end
+    end
 
-#   describe "update" do
-#     it "can update an existing driver with valid information accurately, and redirect" do
-#       # Arrange
-#       # Ensure there is an existing driver saved
-#       # Assign the existing driver's id to a local variable
-#       # Set up the form data
+    it "does not update any driver if given an invalid id, and responds with a 404" do
+      # Arrange
+      # Ensure there is an invalid id that points to no driver
+      # Set up the form data
 
-#       # Act-Assert
-#       # Ensure that there is no change in Driver.count
+      # Act-Assert
+      # Ensure that there is no change in Driver.count
 
-#       # Assert
-#       # Use the local variable of an existing driver's id to find the driver again, and check that its attributes are updated
-#       # Check that the controller redirected the user
+      # Assert
+      # Check that the controller gave back a 404
 
-#     end
+    end
 
-#     it "does not update any driver if given an invalid id, and responds with a 404" do
-#       # Arrange
-#       # Ensure there is an invalid id that points to no driver
-#       # Set up the form data
+    it "does not create a driver if the form data violates Driver validations, and responds with a redirect" do
+      # Note: This will not pass until ActiveRecord Validations lesson
+      # Arrange
+      # Ensure there is an existing driver saved
+      # Assign the existing driver's id to a local variable
+      # Set up the form data so that it violates Driver validations
 
-#       # Act-Assert
-#       # Ensure that there is no change in Driver.count
+      # Act-Assert
+      # Ensure that there is no change in Driver.count
 
-#       # Assert
-#       # Check that the controller gave back a 404
+      # Assert
+      # Check that the controller redirects
 
-#     end
+    end
+  end
 
-#     it "does not create a driver if the form data violates Driver validations, and responds with a redirect" do
-#       # Note: This will not pass until ActiveRecord Validations lesson
-#       # Arrange
-#       # Ensure there is an existing driver saved
-#       # Assign the existing driver's id to a local variable
-#       # Set up the form data so that it violates Driver validations
+  describe "destroy" do
+    it "successfully deletes an existing driver and then redirects to list of drivers at drivers index" do
+        Driver.create(name: "Input driver", phone_num: "1111111111")
+        existing_driver_id = Driver.find_by(name: "Input driver").id
+  
+        expect {
+          delete driver_path( existing_driver_id )
+        }.must_differ "driver.count", -1
+  
+        must_redirect_to drivers_path
+      end
+  end
 
-#       # Act-Assert
-#       # Ensure that there is no change in Driver.count
+     it "does not change the db when the driver does not exist, then responds with " do
+    #   # Arrange
+    #   # Ensure there is an invalid id that points to no driver
+      
 
-#       # Assert
-#       # Check that the controller redirects
 
-#     end
-#   end
+    #   # Act-Assert
+    #   # Ensure that there is no change in Driver.count
 
-#   describe "destroy" do
-#     it "destroys the driver instance in db when driver exists, then redirects" do
-#       # Arrange
-#       # Ensure there is an existing driver saved
+    #   # Assert
+    #   # Check that the controller responds or redirects with whatever your group decides
 
-#       # Act-Assert
-#       # Ensure that there is a change of -1 in Driver.count
-
-#       # Assert
-#       # Check that the controller redirects
-
-#     end
-
-#     it "does not change the db when the driver does not exist, then responds with " do
-#       # Arrange
-#       # Ensure there is an invalid id that points to no driver
-
-#       # Act-Assert
-#       # Ensure that there is no change in Driver.count
-
-#       # Assert
-#       # Check that the controller responds or redirects with whatever your group decides
-
-#     end
-#   end
-# end
+    # end
+  
+end
 
--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -1,31 +1,34 @@
 require "test_helper"
 
 describe Driver do
-  let (:test_driver) {
+  let (:new_driver) {
     Driver.new(name: "Kari", vin: "123", available: true)
   }
   it "can be instantiated" do
     # Assert
-    expect(test_driver.valid?).must_equal true
+    expect(new_driver.valid?).must_equal true
   end
 
+  it "will have the required fields"
+  
 
-  # Tests for methods you create should go here
-  describe "custom methods" do
-    describe "average rating" do
-      # Your code here
-    end
 
-    describe "total earnings" do
-      # Your code here
-    end
+#   # Tests for methods you create should go here
+#   describe "custom methods" do
+#     describe "average rating" do
+#       # Your code here
+#     end
 
-    describe "can go online" do
-      # Your code here
-    end
+#     describe "total earnings" do
+#       # Your code here
+#     end
 
-    describe "can go offline" do
-      # Your code here
-    end
-  end
-end
+#     describe "can go online" do
+#       # Your code here
+#     end
+
+#     describe "can go offline" do
+#       # Your code here
+#     end
+#   end
+# end
