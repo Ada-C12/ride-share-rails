3798f850f0d3febf2e7c88ec120eed5c7b3cd8ac
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-10-11 17:07:03 -0700

    Passing all but one of provided tests.

--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -3,29 +3,31 @@ require "test_helper"
 describe Driver do
   let (:new_driver) {
     Driver.new(name: "Kari", vin: "123", active: true)
+  }
   it "can be instantiated" do
     # Assert
     expect(new_driver.valid?).must_equal true
   end
-
+  
   it "will have the required fields" do
     # Arrange
     new_driver.save
-    driver = Driver.first [:name, :vin, :active].each do |field|
-
+    driver = Driver.first 
+    [:name, :vin, :active].each do |field|
+      
       # Assert
       expect(driver).must_respond_to field
     end
   end
-
+  
   describe "relationships" do
     it "can have many trips" do
       # Arrange
       new_driver.save
-      new_passenger = Passenger.create(name: "Kari", phone_num: "111-111-1211")
+      new_passenger = Passenger.create(name: "Kari", phone_number: "111-111-1211")
       trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5, cost: 1234)
       trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3, cost: 6334)
-
+      
       # Assert
       expect(new_passenger.trips.count).must_equal 2
       new_passenger.trips.each do |trip|
@@ -33,47 +35,47 @@ describe Driver do
       end
     end
   end
-
+  
   describe "validations" do
     it "must have a name" do
       # Arrange
       new_driver.name = nil
-
+      
       # Assert
       expect(new_driver.valid?).must_equal false
       expect(new_driver.errors.messages).must_include :name
       expect(new_driver.errors.messages[:name]).must_equal ["can't be blank"]
     end
-
+    
     it "must have a VIN number" do
       # Arrange
       new_driver.vin = nil
-
+      
       # Assert
       expect(new_driver.valid?).must_equal false
       expect(new_driver.errors.messages).must_include :vin
       expect(new_driver.errors.messages[:vin]).must_equal ["can't be blank"]
     end
   end
-
+  
   # Tests for methods you create should go here
   describe "custom methods" do
     describe "average rating" do
       # Your code here
     end
-
+    
     describe "total earnings" do
       # Your code here
     end
-
+    
     describe "can go online" do
       # Your code here
     end
-
+    
     describe "can go offline" do
       # Your code here
     end
-
+    
     # You may have additional methods to test
   end
 end
