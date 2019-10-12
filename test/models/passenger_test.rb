a15bdb98cd3124befda7c0e8574a3d87ece9df8a
daniela sanchez <dnsanche@gmail.com>
2019-10-11 23:21:24 -0700

    Fixed test for passenger phone number

--- test/models/passenger_test.rb
+++ test/models/passenger_test.rb
@@ -53,8 +53,8 @@ describe Passenger do
 
       # Assert
       expect(new_passenger.valid?).must_equal false
-      expect(new_passenger.errors.messages).must_include :new_passenger
-      expect(new_passenger.errors.messages[:new_passenger]).must_equal ["can't be blank"]
+      expect(new_passenger.errors.messages).must_include :phone_number
+      expect(new_passenger.errors.messages[:phone_number]).must_equal ["can't be blank"]
     end
   end
 
